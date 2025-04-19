b532310246d1c3d562811e900d0e911b00a57bc2
Eelco van Vlietl <eelco@davelab.nl>
2025-04-19 19:11:42 +0200

    added configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,6 +2,8 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
+
+
 { config, lib, pkgs, ... }:
 
 {
@@ -122,6 +124,15 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.05"; # Did you read the comment?
+
+
+  # Algemene systeeminstellingen
+  systemd.services.someService.enable = true;
+  time.timeZone = "Europe/Amsterdam";
+  networking.hostName = "my-host";
+
+  # Schakel zsh in, zoals je eerder had
+  programs.zsh.enable = true;
 
 }
 
