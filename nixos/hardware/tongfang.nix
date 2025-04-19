0c073291698a9dbfaa3c56e7ae7f34eb1f8b4932
Eelco van Vlietl <eelco@davelab.nl>
2025-04-19 18:47:57 +0200

    added root

--- nixos/hardware/tongfang.nix
+++ nixos/hardware/tongfang.nix
@@ -7,7 +7,6 @@
     ./tongfang-graphics-configuration.nix
   ];
 
-
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
@@ -15,7 +14,21 @@
 
   fileSystems."/boot" =
     { 
-      device = "/dev/disk/by-uuid/C22A-626C";:1
-      fsType = "ext4"; options = [ "fmask=0077" "dmask=0077" ];
+      device = "/dev/disk/by-uuid/C22A-626C";
+      fsType = "ext4";
+      options = [ "fmask=0077" "dmask=0077" ];
     };
+
+  # Voeg root bestandssysteem toe
+  fileSystems."/" =
+    {
+      device = "/dev/mapper/luks-f0af0243-70cb-493e-be3c-33a7b3b413ac";  # Je LUKS device
+      fsType = "ext4";
+    };
+
+
+  # Voeg hier de swap-devices en andere opties uit je oude config toe:
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/da00c9e6-cb32-4373-a547-cebf069ab7f1"; }
+  ];
 }
