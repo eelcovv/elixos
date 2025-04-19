71d08ddd5bc8a2977b6428a5b9fe0255e9d66aa3
Eelco van Vlietl <eelco@davelab.nl>
2025-04-19 21:40:49 +0200

    fixed

--- nixos/users/eelco.nix
+++ nixos/users/eelco.nix
@@ -5,7 +5,7 @@
       isNormalUser = true;
       description = "Eelco van Vliet";
       extraGroups = [ "wheel" "networkmanager" "audio" ];
-      hashedPassword = "$6$/BFpWvnMkSUI03E7$wZPqzCZIVxEUdf1L46hkAL.ifLlW61v4iZvWCh9MC5X9UGbRPadOg43AJrw4gfRgWwBRt0u6UxIgmuZ5KuJFo."
+      hashedPassword = "$6$/BFpWvnMkSUI03E7$wZPqzCZIVxEUdf1L46hkAL.ifLlW61v4iZvWCh9MC5X9UGbRPadOg43AJrw4gfRgWwBRt0u6UxIgmuZ5KuJFo.";
       shell = pkgs.zsh;
   };
 }
