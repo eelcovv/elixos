c4368ab8ed3081ada8b604907fcc3361573459b0
Eelco van Vlietl <eelco@davelab.nl>
2025-04-19 17:14:09 +0200

    eerste tongfang hardware setup

--- nixos/hardware/tongfang.nix
+++ nixos/hardware/tongfang.nix
@@ -1,15 +1,24 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, modulesPath, ... }:
 
 {
   imports = [
     # Dit kan een door nixos-generate-config gemaakte hardware config zijn
     ./tongfang-hardware-configuration.nix
+    ./tongfang-hardware-graphics-configurations.nix
   ];
+
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.luks.devices = {
-    "luks-ef0fc5e3-81cc-4d6d-bd1a-f4746a961c2a".device = "/dev/disk/by-uuid/ef0fc5e3-81cc-4d6d-bd1a-f4746a961c2a";
-  };
+      fsType = "ext4";
+    };
+
+  boot.initrd.luks.devices."luks-f0af0243-70cb-493e-be3c-33a7b3b413ac".device = "/dev/disk/by-uuid/f0af0243-70cb-493e-be3c-33a7b3b413ac";
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/C22A-626C";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
 }
