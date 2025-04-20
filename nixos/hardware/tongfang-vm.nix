58fd4051c0d51fd1f4736162ae22fe862872656f
Eelco van Vlietl <eelco@davelab.nl>
2025-04-20 11:04:26 +0200

    weer weg

--- nixos/hardware/tongfang-vm.nix
+++ nixos/hardware/tongfang-vm.nix
@@ -3,6 +3,4 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = false;
-  boot.loader.systemd-boot.configurationLimit = 10;
-  boot.loader.randomSeed.enable = false;  # voorkomt waarschuwing
 }
