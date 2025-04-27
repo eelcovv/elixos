d6e9ef6a4e661ad8b3d493affc1e091ad6562aaf
Eelco van Vlietl <eelco@davelab.nl>
2025-04-27 12:19:43 +0200

    last trail to force group to user

--- nixos/users/eelco.nix
+++ nixos/users/eelco.nix
@@ -19,46 +19,35 @@
  *   - `shell`: Specifies the user's shell, set to Zsh from the package set.
  *   - `openssh.authorizedKeys.keys`: Configures the SSH authorized keys for the user, based on the current hostname.
  */
-{ pkgs, config, ... }:
+ { pkgs, config, ... }:
 
 let
-   # Always familiar keys for all hosts
+  # Always familiar keys for all hosts
   trustedKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC3+DBjLHGlQinS0+qeC5JgFakaPFc+b+btlZABO7ZX6 eelco@tongfang"
-    # possibly more global keys here
   ];
 
-  #Extra keys per specific host
+  # Extra keys per specific host
   hostSpecificKeys = {
-    tongfang = [
-      # For example, extra keys that are only allowed on tongue
-    ];
-    generic-vm = [
-      # For example, test keys that are only allowed on the VM
-    ];
+    tongfang = [];
+    generic-vm = [];
   };
 
-  # The full list: TrustedKeys + Possible host -specific
+  # The full list: TrustedKeys + Possible host-specific
   keys = trustedKeys ++ (hostSpecificKeys.${config.networking.hostName} or []);
 in
 {
   users.users.eelco = {
     isNormalUser = true;
     createHome = true;
+    home = "/home/eelco";
     description = "Eelco van Vliet";
     extraGroups = [ "wheel" "networkmanager" "audio" ];
+    group = "eelco"; # <<< force private group
     hashedPassword = "$6$/BFpWvnMkSUI03E7$wZPqzCZIVxEUdf1L46hkAL.ifLlW61v4iZvWCh9MC5X9UGbRPadOg43AJrw4gfRgWwBRt0u6UxIgmuZ5KuJFo.";
     shell = pkgs.zsh;
-    # openssh.authorizedKeys.keys = keys; 
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC3+DBjLHGlQinS0+qeC5JgFakaPFc+b+btlZABO7ZX6 eelco@tongfang"
-    ];
+    openssh.authorizedKeys.keys = keys;
   };
 
-  # Zorg dat de juiste permissions en ownership bestaan voor .ssh
-  systemd.tmpfiles.rules = [
-    "d /home/eelco/.ssh 0700 eelco eelco - -"
-    "f /home/eelco/.ssh/authorized_keys 0600 eelco eelco - -"
-  ];
-
+  users.groups.eelco = {}; # <<< define group "eelco"
 }
