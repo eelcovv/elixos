ab4318b440e1b9ef5d550fc9fdf472543910d727
Eelco van Vlietl <eelco@davelab.nl>
2025-04-27 14:53:06 +0200

    back to default group

--- nixos/users/eelco.nix
+++ nixos/users/eelco.nix
@@ -28,9 +28,7 @@
     home = "/home/eelco";
     description = "Eelco van Vliet";
     extraGroups = [ "wheel" "networkmanager" "audio" ];
-    group = "eelco"; # <<< force private group
     hashedPassword = "$6$/BFpWvnMkSUI03E7$wZPqzCZIVxEUdf1L46hkAL.ifLlW61v4iZvWCh9MC5X9UGbRPadOg43AJrw4gfRgWwBRt0u6UxIgmuZ5KuJFo.";
     shell = pkgs.zsh;
   };
-
 }
