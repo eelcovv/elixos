32900c9494b39e18e4e6bb7739a3f6d952a32a35
Eelco van Vlietl <eelco@davelab.nl>
2025-04-27 12:43:57 +0200

    turned off unused services

--- nixos/modules/services/generic-vm.nix
+++ nixos/modules/services/generic-vm.nix
@@ -1,19 +1,26 @@
 {
-  services.xserver = {
-    enable = true;
-    videoDrivers = [ "virtio" ];
-  };
 
-  services.openssh = {
-    enable = true;
+  services = {
 
-    # Alleen inloggen met public key, geen wachtwoord
-    settings = {
-      PasswordAuthentication = false;
-      PermitRootLogin = "no";
+    dbus.enable = false;
+    bluetooth.enable = false;
+
+    xserver = {
+      enable = true;
+      videoDrivers = [ "virtio" ];
     };
 
-    # Open de poort in de firewall (voor als je die ooit inschakelt)
-    openFirewall = true;
+    openssh = {
+      enable = true;
+
+      # Alleen inloggen met public key, geen wachtwoord
+      settings = {
+        PasswordAuthentication = false;
+        PermitRootLogin = "no";
+      };
+
+      # Open de poort in de firewall (voor als je die ooit inschakelt)
+      openFirewall = true;
+    };
   };
 }
