b2205508ab610ff0c78402648f4c59ad9eb32094
Eelco van Vlietl <eelco@davelab.nl>
2025-05-06 22:33:51 +0200

    added fall back

--- nixos/users/authorized_keys.nix
+++ nixos/users/authorized_keys.nix
@@ -2,28 +2,23 @@
 
 let
   trustedKeys = [
-    # Global trusted keys
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC3+DBjLHGlQinS0+qeC5JgFakaPFc+b+btlZABO7ZX6 eelco@tongfang"
-    # Possible more keys, if you want to run VM on other machines too
   ];
 
   hostSpecificKeys = {
-    tongfang = [
-      # Extra keys only for Tongfang
-    ];
-    generic-vm = [
-      # Extra keys only for the VM
-    ];
+    tongfang = [];
+    generic-vm = [];
   };
 
-  allKeys = trustedKeys ++ (hostSpecificKeys.${config.networking.hostName} or [ ]);
+  host = config.networking.hostName or null;
+  allKeys = trustedKeys ++ (hostSpecificKeys.${host} or []);
 in
 {
-   options.eelco-authorized-keys= lib.mkOption {
-      type = lib.types.listOf lib.types.str;
-      default = allKeys;
-      description = "Authorized SSH keys for user Eelco based on trusted and host-specific keys.";
+  options.eelco-authorized-keys = lib.mkOption {
+    type = lib.types.listOf lib.types.str;
+    default = allKeys;
+    description = "Authorized SSH keys for user Eelco based on trusted and host-specific keys.";
   };
-    config.eelco-authorized-keys = allKeys;
 
+  config.eelco-authorized-keys = allKeys;
 }
