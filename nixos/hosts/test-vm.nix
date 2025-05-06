952c4951facbe6bd1756f32ea92608333f03d635
Eelco van Vlietl <eelco@davelab.nl>
2025-05-06 23:17:19 +0200

    added zsh

--- nixos/hosts/test-vm.nix
+++ nixos/hosts/test-vm.nix
@@ -1,27 +1,21 @@
+{ config, pkgs, ... }:
+
 {
-  description = "Minimal test system";
+  networking.hostName = "test-vm";
+  services.openssh.enable = true;
+  programs.zsh.enable = true;
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-  outputs = { nixpkgs, ... }: {
-    nixosConfigurations.test-vm = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        {
-          networking.hostName = "test-vm";
-          services.openssh.enable = true;
-          users.users.eelco = {
-            isNormalUser = true;
-            createHome = true;
-            home = "/home/eelco";
-            shell = nixpkgs.legacyPackages.x86_64-linux.zsh;
-            openssh.authorizedKeys.keys = [
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC3+DBjLHGlQinS0+qeC5JgFakaPFc+b+btlZABO7ZX6 eelco@tongfang"
-            ];
-          };
-          system.stateVersion = "24.11";
-        }
-      ];
-    };
+  users.users.eelco = {
+    isNormalUser = true;
+    createHome = true;
+    home = "/home/eelco";
+    password = "test123";
+    shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC3+DBjLHGlQinS0+qeC5JgFakaPFc+b+btlZABO7ZX6 eelco@tongfang"
+    ];
   };
+
+  system.stateVersion = "24.11";
 }
+
