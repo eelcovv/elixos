67453298e80c809a56dbb4be2588fd44f2a0ca27
Eelco van Vlietl <eelco@davelab.nl>
2025-05-07 00:23:15 +0200

    added structure

--- nixos/users/authorized_keys.nix
+++ nixos/users/authorized_keys.nix
@@ -12,13 +12,38 @@ let
 
   host = config.networking.hostName or null;
   allKeys = trustedKeys ++ (hostSpecificKeys.${host} or []);
-in
-{
-  options.eelco-authorized-keys = lib.mkOption {
-    type = lib.types.listOf lib.types.str;
-    default = allKeys;
-    description = "Authorized SSH keys for user Eelco based on trusted and host-specific keys.";
+
+  tmpfilesForAuthorizedKeys = [
+    "d /home/eelco/.ssh 0700 eelco users"
+  ] ++ map (key:
+    "f /home/eelco/.ssh/authorized_keys 0600 eelco users - ${key}"
+  ) allKeys;
+
+in {
+  options = {
+    eelco-authorized-keys = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = allKeys;
+      description = "Authorized SSH keys for user Eelco based on trusted and host-specific keys.";
+    };
+
+    # This option is used to set up tmpfiles rules for SSH authorized_keys
+    # for the user Eelco. It ensures that the authorized_keys file is created
+    # with the correct permissions and ownership.
+    # The default value is derived from the authorized keys.
+    # This approach is required because the approach with
+    # users.users.eelco.openssh.authorizedKeys.keys = [ "...key..." ]; 
+    # fails to create the authorized_keys file. This is a known issue 
+    eelco-tmpfiles-ssh-setup = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = tmpfilesForAuthorizedKeys;
+      description = "Tmpfiles rules to install authorized_keys for Eelco.";
+    };
   };
 
-  config.eelco-authorized-keys = builtins.trace allKeys allKeys;
+  config = {
+    eelco-authorized-keys = allKeys;
+    eelco-tmpfiles-ssh-setup = tmpfilesForAuthorizedKeys;
+  };
 }
+
