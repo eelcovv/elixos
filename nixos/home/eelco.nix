8b24673007d2e1bef02ca2d837e9c854b288b23c
Eelco van Vlietl <eelco@davelab.nl>
2025-05-10 11:00:18 +0200

    fixed home

--- nixos/home/eelco.nix
+++ nixos/home/eelco.nix
@@ -1,9 +1,12 @@
+{ config, pkgs, lib, ... }:
+
 {
   home-manager.users.eelco = {
     home.stateVersion = "24.11";
 
     imports = [
       (import ./modules/git.nix {
+        inherit config pkgs lib;
         userName = "Eelco van Vliet";
         userEmail = "eelcovv@gmail.com";
       })
