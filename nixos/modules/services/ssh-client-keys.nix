09eb8ec4fd869c2e78a523a31077f494426e3f58
Eelco van Vlietl <eelco@davelab.nl>
2025-05-11 18:20:32 +0200

    replaced with multi user version

--- nixos/modules/services/ssh-client-keys.nix
+++ nixos/modules/services/ssh-client-keys.nix
@@ -1,17 +1,32 @@
 { config, lib, pkgs, ... }:
 
+let
+  sshUsersRaw = config.globalSshClientUsers or [];
+
+  # Filter alleen bestaande users
+  sshUsers = lib.filter (user: config.users.users ? ${user}) sshUsersRaw;
+in
 {
-  systemd.services.generate-ssh-pubkey = {
-    description = "Generate id_ed25519.pub from id_ed25519";
+  systemd.services = lib.genAttrs sshUsers (user: {
+    description = "Generate SSH public key from private key for ${user}";
     wantedBy = [ "multi-user.target" ];
+    after = [ "default.target" ];
     serviceConfig = {
       Type = "oneshot";
-      User = "eelco";
+      User = user;
       ExecStart = ''
-        if [ -f /home/eelco/.ssh/id_ed25519 ]; then
-          ${pkgs.openssh}/bin/ssh-keygen -y -f /home/eelco/.ssh/id_ed25519 > /home/eelco/.ssh/id_ed25519.pub
+        if [ -f /home/${user}/.ssh/id_ed25519 ]; then
+          ${pkgs.openssh}/bin/ssh-keygen -y -f /home/${user}/.ssh/id_ed25519 > /home/${user}/.ssh/id_ed25519.pub
         fi
       '';
+      ExecStartPre = "! test -s /home/${user}/.ssh/id_ed25519.pub";
     };
-  };
+  });
+
+  systemd.tmpfiles.rules = sshUsers
+    ++ [
+      # Zorg dat .ssh directory bestaat
+      "d /home/eelco/.ssh 0700 eelco users -"
+      "f /home/eelco/.ssh/id_ed25519.pub 0644 eelco users -"
+    ];
 }
