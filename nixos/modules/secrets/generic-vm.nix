f80f0d7582d7c52b091c06d4b820654e4fceb527
Eelco van Vlietl <eelco@davelab.nl>
2025-05-16 21:59:45 +0200

    changed link to secrets

--- nixos/modules/secrets/generic-vm.nix
+++ nixos/modules/secrets/generic-vm.nix
@@ -1,5 +1,5 @@
 {
-  sops.defaultSopsFile = ../../secrets/generic-vm-secrets.yaml;
+  sops.defaultSopsFile = ../../secrets/generic-vm-eelco-secrets.yaml;
   sops.age.keyFile = "/etc/sops/age/keys.txt";  
 
   sops.secrets.age_key = {
