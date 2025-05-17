719a3f0809667814ffa69c264993a43b04bb988d
Eelco van Vlietl <eelco@davelab.nl>
2025-05-18 00:20:38 +0200

    complete

--- nixos/disks/singer.nix
+++ nixos/disks/singer.nix
@@ -1,33 +1,40 @@
-disk."main" = {
-  type = "disk";
-  device = "/dev/sda";
-  content = {
-    type = "gpt";
-    partitions = {
-      ESP = {
-        start = "1MiB";
-        end = "513MiB";
-        type = "EF00";
-        content = {
-          type = "filesystem";
-          format = "vfat";
-          mountpoint = "/boot";
-        };
-      };
-      root = {
-        start = "513MiB";
-        content = {
-          type = "luks";
-          name = "crypt";
-          settings.allowDiscards = true;
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
+
+{ config, lib, ... }:
+
+{
+  disko.devices = {
+    disk.main = {
+      type = "disk";
+      device = "/dev/sda";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            start = "1MiB";
+            end = "513MiB";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+          root = {
+            start = "513MiB";
+            # end wordt automatisch tot einde disk
+            content = {
+              type = "luks";
+              name = "crypt";
+              settings.allowDiscards = true;
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
           };
         };
       };
     };
   };
-};
-
+}
