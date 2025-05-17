c9b5e6585adc22e2e01ba4725020dbc034d351c5
Eelco van Vlietl <eelco@davelab.nl>
2025-05-17 12:04:45 +0200

    fixed it again

--- nixos/modules/secrets/generic-vm-eelco.nix
+++ nixos/modules/secrets/generic-vm-eelco.nix
@@ -1,41 +1,38 @@
 { config, pkgs, lib, ... }:
 
 {
+  # De versleutelde secrets.yaml
   sops.defaultSopsFile = ../../secrets/generic-vm-eelco-secrets.yaml;
 
-  sops.age.keyFile = "/etc/sops/age/keys.txt";
+  # Haal age key uit 'age_key' binnen de secrets
+  sops.age.keySources = [ "sops://age_key" ];
 
-  sops.secrets.age_key = {
-    path = "/etc/sops/age/keys.txt";
-    owner = "root";
-    group = "root";
-    mode = "0400";
-  };
-
+  # Secret voor de private SSH key
   sops.secrets.id_ed25519 = {
     path = "/home/eelco/.ssh/id_ed25519";
     owner = "eelco";
     group = "users";
     mode = "0400";
+    restartUnits = [ "generate-ssh-pubkey.service" ];
   };
 
+  # Zorg dat ~/.ssh bestaat
+  systemd.tmpfiles.rules = [
+    "d /home/eelco/.ssh 0700 eelco users -"
+  ];
+
+  # Genereer automatisch de .pub na decryptie van id_ed25519
   systemd.services.generate-ssh-pubkey = {
     description = "Generate SSH public key from decrypted id_ed25519";
     wantedBy = [ "multi-user.target" ];
-    after = [ "sops-nix.service" ];
-    requires = [ "sops-nix.service" ];
+    after = [ "sops-nix-id_ed25519.service" ];
+    requires = [ "sops-nix-id_ed25519.service" ];
     serviceConfig = {
       Type = "oneshot";
       User = "eelco";
       ExecStart = "${pkgs.writeShellScript "generate-pubkey" ''
-  ssh-keygen -y -f /home/eelco/.ssh/id_ed25519 > /home/eelco/.ssh/id_ed25519.pub
-''}";
-
-
+        ssh-keygen -y -f /home/eelco/.ssh/id_ed25519 > /home/eelco/.ssh/id_ed25519.pub
+      ''}";
     };
   };
-
-  systemd.tmpfiles.rules = [
-    "d /home/eelco/.ssh 0700 eelco users -"
-  ];
 }
