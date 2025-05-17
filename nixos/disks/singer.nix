01a10cdc5e59238dd4582fafe1b241ac058fb4f9
Eelco van Vlietl <eelco@davelab.nl>
2025-05-18 00:19:27 +0200

    changed names

--- nixos/disks/singer.nix
+++ nixos/disks/singer.nix
@@ -1,37 +1,33 @@
-
-{
-  disko.devices = {
-    disk.main = {
-      type = "disk";
-      device = "/dev/sda";
-      content = {
-        type = "gpt";
-        partitions = {
-          ESP = {
-            start = "1MiB";
-            end = "513MiB";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-            };
-          };
-          root = {
-            start = "513MiB";
-            content = {
-              type = "luks";
-              name = "crypted";
-              settings.allowDiscards = true;
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
-            };
+disk."main" = {
+  type = "disk";
+  device = "/dev/sda";
+  content = {
+    type = "gpt";
+    partitions = {
+      ESP = {
+        start = "1MiB";
+        end = "513MiB";
+        type = "EF00";
+        content = {
+          type = "filesystem";
+          format = "vfat";
+          mountpoint = "/boot";
+        };
+      };
+      root = {
+        start = "513MiB";
+        content = {
+          type = "luks";
+          name = "crypt";
+          settings.allowDiscards = true;
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/";
           };
         };
       };
     };
   };
-}
+};
+
