2edb09cae93e38782f25662abe56eef42f0e662e
Eelco van Vlietl <eelco@davelab.nl>
2025-05-26 00:23:14 +0200

    added default

--- nixos/modules/secrets/default.nix
+++ nixos/modules/secrets/default.nix
@@ -5,13 +5,14 @@
     "d /etc/sops/age 0700 root root -"
   ];
 
-  sops.secrets.age_key = {
-    sopsFile = ../../secrets/age_key.yaml;
-    path = "/etc/sops/age/keys.txt";
-    owner = "root";
-    group = "root";
-    mode = "0400";
+  sops = {
+    defaultSopsFile = ../../secrets/age_key.yaml;
+    age.keyFile = "/etc/sops/age/keys.txt";
+    secrets.age_key = {
+      path = "/etc/sops/age/keys.txt";
+      owner = "root";
+      group = "root";
+      mode = "0400";
+    };
   };
-
-  sops.age.keyFile = "/etc/sops/age/keys.txt"; # This creates the symbolic link to your sops age keys.txt
 }
