a1f52132df7ba3427e24976954be510d9781c844
Eelco van Vlietl <eelco@davelab.nl>
2025-05-25 23:51:24 +0200

    changed ordere

--- nixos/modules/secrets/bootstrap-agekey.nix
+++ nixos/modules/secrets/bootstrap-agekey.nix
@@ -6,6 +6,9 @@
     umask 077
     mkdir -p /etc/sops/age
 
+    export SOPS_AGE_KEY_FILE="/root/.config/sops/age/keys.txt"
+    export HOME="/root"
+
     if [ ! -f "$SOPS_AGE_KEY_FILE" ]; then
       echo "❌ SOPS_AGE_KEY_FILE ($SOPS_AGE_KEY_FILE) does not exist!"
       exit 1
@@ -13,9 +16,7 @@
 
     echo "📁 Using SOPS_AGE_KEY_FILE at: $SOPS_AGE_KEY_FILE"
 
-    HOME=/root \
-    SOPS_AGE_KEY_FILE=/root/.config/sops/age/keys.txt \
-      ${pkgs.sops}/bin/sops -d ${../../secrets/age_key.yaml} |
+    ${pkgs.sops}/bin/sops -d ${../../secrets/age_key.yaml} |
       ${pkgs.yq}/bin/yq -r .age_key > /etc/sops/age/keys.txt
   '';
 }
