6ea90df692917a17bcb31d987ec448b7a421ea69
Eelco van Vlietl <eelco@davelab.nl>
2025-05-25 21:58:53 +0200

    try with new setup

--- nixos/modules/secrets/default.nix
+++ nixos/modules/secrets/default.nix
@@ -1,13 +1,13 @@
 { config, pkgs, lib, ... }:
 
 {
-  sops.age.keyFile = "/etc/sops/age/keys.txt";
-
   sops.secrets.age_key = {
-    sopsFile = ../../secrets/singer-eelco-secrets.yaml;
+    sopsFile = ../../secrets/age_key.yaml;
     path = "/etc/sops/age/keys.txt";
     owner = "root";
     group = "root";
     mode = "0400";
   };
+
+  sops.age.keyFile = "/etc/sops/age/keys.txt"; # This creates the symbolic link to your sops age keys.txt
 }
