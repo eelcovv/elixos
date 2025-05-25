ed0d3682f924415a2f6a4f69f41b3da9fe051105
Eelco van Vlietl <eelco@davelab.nl>
2025-05-25 23:32:28 +0200

    added sops key

--- nixos/modules/secrets/bootstrap-agekey.nix
+++ nixos/modules/secrets/bootstrap-agekey.nix
@@ -1,11 +1,13 @@
 { config, pkgs, lib, ... }:
 
 {
-  # This encrypts the age key itself and stores it in a expected place
   system.activationScripts.installAgeKey.text = ''
     echo "🔐 Installing /etc/sops/age/keys.txt using in-store sops"
     umask 077
     mkdir -p /etc/sops/age
-    ${pkgs.sops}/bin/sops -d ${../../secrets/age_key.yaml} | ${pkgs.yq}/bin/yq -r .age_key > /etc/sops/age/keys.txt
+
+    SOPS_AGE_KEY_FILE=/etc/sops/age/keys.txt \
+      ${pkgs.sops}/bin/sops -d ${../../secrets/age_key.yaml} | \
+      ${pkgs.yq}/bin/yq -r .age_key > /etc/sops/age/keys.txt
   '';
 }
