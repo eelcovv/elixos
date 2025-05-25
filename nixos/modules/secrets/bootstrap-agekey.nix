8eb1d9432fde97c8dbab79028c902db54f3126eb
Eelco van Vlietl <eelco@davelab.nl>
2025-05-25 23:36:32 +0200

    geti is from root

--- nixos/modules/secrets/bootstrap-agekey.nix
+++ nixos/modules/secrets/bootstrap-agekey.nix
@@ -2,12 +2,13 @@
 
 {
   system.activationScripts.installAgeKey.text = ''
-    echo "🔐 Installing /etc/sops/age/keys.txt using in-store sops"
+    echo "🔐 Installing /etc/sops/age/keys.txt using local identity"
     umask 077
     mkdir -p /etc/sops/age
 
-    SOPS_AGE_KEY_FILE=/etc/sops/age/keys.txt \
-      ${pkgs.sops}/bin/sops -d ${../../secrets/age_key.yaml} | \
+    HOME=/root \
+    SOPS_AGE_KEY_FILE=/root/.config/sops/age/keys.txt \
+      ${pkgs.sops}/bin/sops -d ${../../secrets/age_key.yaml} |
       ${pkgs.yq}/bin/yq -r .age_key > /etc/sops/age/keys.txt
   '';
 }
