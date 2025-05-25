a39dbcc616ac4106c75b88cb81936c0443b22ea1
Eelco van Vlietl <eelco@davelab.nl>
2025-05-26 00:25:33 +0200

    last attempts

--- nixos/modules/secrets/default.nix
+++ nixos/modules/secrets/default.nix
@@ -5,6 +5,10 @@
     "d /etc/sops/age 0700 root root -"
   ];
 
+  systemd.user.extraEnv = {
+    SOPS_AGE_KEY_FILE = "/etc/sops/age/keys.txt";
+  };
+
   sops = {
     defaultSopsFile = ../../secrets/age_key.yaml;
     age.keyFile = "/etc/sops/age/keys.txt";
