0a96f17349507dbb8b7a0fe2d33e65a0322113ff
Eelco van Vlietl <eelco@davelab.nl>
2025-05-26 00:31:32 +0200

    hacking

--- nixos/modules/secrets/default.nix
+++ nixos/modules/secrets/default.nix
@@ -1,21 +1,23 @@
 { config, pkgs, lib, ... }:
 
 {
+  # Zorg dat de doelmap bestaat
   systemd.tmpfiles.rules = [
     "d /etc/sops/age 0700 root root -"
   ];
 
-  environment.variables.SOPS_AGE_KEY_FILE = "/etc/sops/age/keys.txt";
+  # Laat Nix weten waar de age key naartoe moet
+  sops.age.keyFile = "/etc/sops/age/keys.txt";
 
-  sops = {
-    age.keyFile = "/etc/sops/age/keys.txt";
+  # SOPS_AGE_KEY_FILE beschikbaar maken voor systemd services
+  systemd.services."sops-install-secrets".environment.SOPS_AGE_KEY_FILE = "/etc/sops/age/keys.txt";
 
-    secrets.age_key = {
-      sopsFile = ../../secrets/age_key.yaml;
-      path = "/etc/sops/age/keys.txt";
-      owner = "root";
-      group = "root";
-      mode = "0400";
-    };
+  # Deze decrypt de age_key.yaml → /etc/sops/age/keys.txt
+  sops.secrets.age_key = {
+    sopsFile = ../../secrets/age_key.yaml;
+    path = "/etc/sops/age/keys.txt";
+    owner = "root";
+    group = "root";
+    mode = "0400";
   };
 }
