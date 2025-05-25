d54ef84cad7ce650b10f52043824670687db15cf
Eelco van Vlietl <eelco@davelab.nl>
2025-05-25 23:30:33 +0200

    absolute path

--- nixos/modules/secrets/bootstrap-agekey.nix
+++ nixos/modules/secrets/bootstrap-agekey.nix
@@ -1,11 +1,9 @@
-{ pkgs, lib, ... }:
-
 {
   # This encrypts the age key itself and stores it in a expected place
   system.activationScripts.installAgeKey.text = ''
-    echo "🔐 Installing /etc/sops/age/keys.txt..."
+    echo "🔐 Installing /etc/sops/age/keys.txt using in-store sops"
     umask 077
     mkdir -p /etc/sops/age
-    sops -d ${../../secrets/age_key.yaml} | yq -r .age_key > /etc/sops/age/keys.txt
+    ${pkgs.sops}/bin/sops -d ${../../secrets/age_key.yaml} | ${pkgs.yq}/bin/yq -r .age_key > /etc/sops/age/keys.txt
   '';
 }
