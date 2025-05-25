9fe7dee651163286bf3033d40794a34dccb1ce01
Eelco van Vlietl <eelco@davelab.nl>
2025-05-25 20:44:11 +0200

    added my sops

--- nixos/modules/secrets/default.nix
+++ nixos/modules/secrets/default.nix
@@ -4,6 +4,7 @@
   sops.age.keyFile = "/etc/sops/age/keys.txt";
 
   sops.secrets.age_key = {
+    sopsFile = ../../secrets/singer-eelco-secrets.yaml;
     path = "/etc/sops/age/keys.txt";
     owner = "root";
     group = "root";
