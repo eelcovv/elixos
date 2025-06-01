49bc00d19e97538222ebb640b0ee9d875e5db614
Eelco van Vlietl <eelco@davelab.nl>
2025-06-01 08:59:08 +0200

    added sshusers option

--- nixos/modules/services/ssh-client-keys.nix
+++ nixos/modules/services/ssh-client-keys.nix
@@ -1,48 +1,56 @@
-{ config, lib, pkgs, ... }:
-
-let
-  sshUsers = config.sshUsers or [];
-
-  hasSecretFile = user:
-    builtins.pathExists (../../secrets + "/${config.networking.hostName}-${user}-secrets.yaml");
-
-  validUsers = builtins.filter hasSecretFile sshUsers;
-
-  userSecret = user: {
-    "id_ed25519_${user}" = {
-      sopsFile = ../../secrets/${config.networking.hostName}-${user}-secrets.yaml;
-      path = "/home/${user}/.ssh/id_ed25519";
-      owner = user;
-      group = "users";
-      mode = "0400";
-      restartUnits = [ "generate-ssh-pubkey-${user}.service" ];
-    };
+{
+  options.sshUsers = lib.mkOption {
+    type = with lib.types; listOf str;
+    default = [ ];
+    description = "List of users for whom SSH keys should be provisioned via SOPS.";
   };
 
-  userService = user: {
-    "generate-ssh-pubkey-${user}" = {
-      description = "Generate SSH public key from private key for ${user}";
-      wantedBy = [ "multi-user.target" ];
-      after = [ "sops-nix-id_ed25519_${user}.service" ];
-      requires = [ "sops-nix-id_ed25519_${user}.service" ];
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = pkgs.writeShellScript "generate-id-ed25519-pub-${user}" ''
-          if [ ! -f /home/${user}/.ssh/id_ed25519.pub ]; then
-            ssh-keygen -y -f /home/${user}/.ssh/id_ed25519 > /home/${user}/.ssh/id_ed25519.pub
-            chown ${user}:users /home/${user}/.ssh/id_ed25519.pub
-            chmod 0644 /home/${user}/.ssh/id_ed25519.pub
-          fi
-        '';
+  config =
+
+    let
+      sshUsers = config.sshUsers or [];
+
+      hasSecretFile = user:
+        builtins.pathExists (../../secrets + "/${config.networking.hostName}-${user}-secrets.yaml");
+
+      validUsers = builtins.filter hasSecretFile sshUsers;
+
+      userSecret = user: {
+        "id_ed25519_${user}" = {
+          sopsFile = ../../secrets/${config.networking.hostName}-${user}-secrets.yaml;
+          path = "/home/${user}/.ssh/id_ed25519";
+          owner = user;
+          group = "users";
+          mode = "0400";
+          restartUnits = [ "generate-ssh-pubkey-${user}.service" ];
+        };
       };
-    };
-  };
 
-in {
-  sops.secrets = lib.mkMerge (map userSecret validUsers);
-  systemd.services = lib.mkMerge (map userService validUsers);
-  systemd.tmpfiles.rules = lib.flatten (map (user: [
-    "d /home/${user}/.ssh 0700 ${user} users -"
-    "f /home/${user}/.ssh/id_ed25519.pub 0644 ${user} users -"
-  ]) validUsers);
+      userService = user: {
+        "generate-ssh-pubkey-${user}" = {
+          description = "Generate SSH public key from private key for ${user}";
+          wantedBy = [ "multi-user.target" ];
+          after = [ "sops-nix-id_ed25519_${user}.service" ];
+          requires = [ "sops-nix-id_ed25519_${user}.service" ];
+          serviceConfig = {
+            Type = "oneshot";
+            ExecStart = pkgs.writeShellScript "generate-id-ed25519-pub-${user}" ''
+              if [ ! -f /home/${user}/.ssh/id_ed25519.pub ]; then
+                ssh-keygen -y -f /home/${user}/.ssh/id_ed25519 > /home/${user}/.ssh/id_ed25519.pub
+                chown ${user}:users /home/${user}/.ssh/id_ed25519.pub
+                chmod 0644 /home/${user}/.ssh/id_ed25519.pub
+              fi
+            '';
+          };
+        };
+      };
+    in {
+      sops.secrets = lib.mkMerge (map userSecret validUsers);
+      systemd.services = lib.mkMerge (map userService validUsers);
+      systemd.tmpfiles.rules = lib.flatten (map (user: [
+        "d /home/${user}/.ssh 0700 ${user} users -"
+        "f /home/${user}/.ssh/id_ed25519.pub 0644 ${user} users -"
+      ]) validUsers);
+    };
 }
+
