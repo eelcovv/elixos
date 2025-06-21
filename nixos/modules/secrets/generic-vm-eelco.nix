64ada14f4ecf1a710128f9197b0d2f878a71a37d
Eelco van Vlietl <eelco@davelab.nl>
2025-06-21 16:00:13 +0200

    added new one shtot

--- nixos/modules/secrets/generic-vm-eelco.nix
+++ nixos/modules/secrets/generic-vm-eelco.nix
@@ -12,21 +12,26 @@
     restartUnits = [ "generate-ssh-pubkey.service" ];
   };
 
-  # Zorg dat ~/.ssh bestaat
+  # Make sure that  ~/.ssh exists and has the right permissions
   systemd.tmpfiles.rules = lib.mkBefore [
     "d /home/eelco/.ssh 0700 eelco users -"
   ];
 
-  # Genereer automatisch de .pub na decryptie van id_ed25519
+  # Automatically generate the .pub file after decryption of id_ed25519
   systemd.services.generate-ssh-pubkey = {
-    description = "Generate SSH public key from decrypted id_ed25519";
-    requires = [ "sops-nix-id_ed25519_eelco.service" ];
-    serviceConfig = {
-      Type = "oneshot";
-      User = "eelco";
-      ExecStart = "${pkgs.writeShellScript "generate-pubkey" ''
-        ssh-keygen -y -f /home/eelco/.ssh/id_ed25519 > /home/eelco/.ssh/id_ed25519.pub
-      ''}";
-    };
+  description = "Generate SSH public key from decrypted id_ed25519";
+  requires = [ "sops-nix-id_ed25519_eelco.service" ];
+  after = [ "sops-nix-id_ed25519_eelco.service" ];
+  wantedBy = [ "default.target" ];  # optioneel
+  serviceConfig = {
+    Type = "oneshot";
+    User = "eelco";
+    ExecStartPre = "${pkgs.coreutils}/bin/test -s /home/eelco/.ssh/id_ed25519"; # alleen starten als bestand niet leeg is
+    ExecStart = "${pkgs.writeShellScript "generate-pubkey" ''
+      set -e
+      ssh-keygen -y -f /home/eelco/.ssh/id_ed25519 > /home/eelco/.ssh/id_ed25519.pub
+    ''}";
   };
+};
+
 }
