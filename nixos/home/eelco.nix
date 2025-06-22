48f16851817e2ba3a8c42dc597aff79d9e34523f
Eelco van Vlietl <eelco@davelab.nl>
2025-06-22 20:09:22 +0200

    updated git

--- nixos/home/eelco.nix
+++ nixos/home/eelco.nix
@@ -5,12 +5,8 @@
     home.stateVersion = "24.11";
 
     imports = [
-      (import ./modules/git.nix {
-        inherit config pkgs lib;
-        userName = "Eelco van Vliet";
-        userEmail = "eelcovv@gmail.com";
-      })
       ./modules/inputrc.nix
+      ./modules/git.nix
       ./modules/zsh.nix
       ./modules/common-packages.nix
     ];
