2ca57b4be0e1b46b1b1a88157d3fc0bdb6e899ea
Eelco van Vlietl <eelco@davelab.nl>
2025-06-24 00:39:15 +0200

    added por

--- nixos/hosts/generic-vm.nix
+++ nixos/hosts/generic-vm.nix
@@ -31,6 +31,7 @@
     ../modules/profiles/desktop.nix
     ../modules/home-manager.nix
     ../users/eelco.nix
+    ../users/por.nix
     ../modules/secrets/default.nix
     ../modules/secrets/generic-vm-eelco.nix
     ../modules/services/ssh-client-keys.nix
