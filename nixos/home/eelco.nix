e75e62cbb4c7089a6cbaf34c456867370cca9f4c
Eelco van Vlietl <eelco@davelab.nl>
2025-06-23 23:33:53 +0200

    direct import

--- nixos/home/eelco.nix
+++ nixos/home/eelco.nix
@@ -6,11 +6,7 @@
   home.stateVersion = "24.11";
 
   imports = [
-    (import ./modules/git.nix {
-      inherit config pkgs lib;
-      userName = "Eelco van Vliet";
-      userEmail = "eelcovv@gmail.com";
-    })
+    ./modules/git.nix
     ./modules/inputrc.nix
     ./modules/zsh.nix
     ./modules/common-packages.nix
