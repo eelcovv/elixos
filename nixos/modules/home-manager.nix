4b9495094e91027f97dfdd7543373787992d6c39
Eelco van Vlietl <eelco@davelab.nl>
2025-06-24 00:53:03 +0200

    fixed

--- nixos/modules/home-manager.nix
+++ nixos/modules/home-manager.nix
@@ -1,13 +1,10 @@
-{ config, lib, pkgs, inputs, ... }:
-
 let
-  userPath = ../../home/users;
-
+  userPath = ../home/users;
   userConfigs = lib.genAttrs config.configuredUsers (user:
     let
       path = userPath + "/${user}.nix";
     in
-      if builtins.pathExists path then import path
+      if builtins.pathExists path then import (toString path)
       else throw "Home Manager config not found for user '${user}' at ${toString path}"
   );
 in
