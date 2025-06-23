d321805a66b1be2cdda5c326625985b8c2172721
Eelco van Vlietl <eelco@davelab.nl>
2025-06-24 00:56:25 +0200

    try

--- nixos/modules/home-manager.nix
+++ nixos/modules/home-manager.nix
@@ -1,24 +1,16 @@
-{ config, lib, pkgs, inputs, ... }:
+{ config, lib, pkgs, inputs, userModulesPath, ... }:
 
 let
-  # Pad naar de gebruikersconfiguraties
-  userPath = ../../home/users;
-
-  # Genereer een attribuutset van alle gebruikers in `configuredUsers`
   userConfigs = lib.genAttrs config.configuredUsers (user:
     let
-      path = userPath + "/${user}.nix";
+      path = userModulesPath + "/${user}.nix";
     in
       if builtins.pathExists path then import (toString path)
       else throw "Home Manager config not found for user '${user}' at ${toString path}"
   );
 in
 {
-  # Home Manager integratie met NixOS
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
-
-  # Alleen voor opgegeven gebruikers
   home-manager.users = userConfigs;
 }
-
