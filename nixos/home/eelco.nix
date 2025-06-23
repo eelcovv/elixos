7edb9c7778aa5c3568c5fce6a6fba003ea865c9c
Eelco van Vlietl <eelco@davelab.nl>
2025-06-23 23:50:08 +0200

    another update

--- nixos/home/eelco.nix
+++ nixos/home/eelco.nix
@@ -1,19 +1,18 @@
 { config, pkgs, lib, ... }:
 
 {
-  home-manager.users.eelco = {
-    home.stateVersion = "24.11";
+  home.username = "eelco";
+  home.homeDirectory = "/home/eelco";
+  home.stateVersion = "24.05"; 
 
-    imports = [
-      ./modules/common-packages.nix
+  imports = [
+    ./modules/common-packages.nix
 
-      # Personal overrides
-      (import ./modules/git.nix {
-        inherit config pkgs lib;
-        userName = "Eelco van Vliet";
-        userEmail = "eelcovv@gmail.com";
-      })
-    ];
-  };
+    # Personal overrides
+    (import ./modules/git.nix {
+      inherit config pkgs lib;
+      userName = "Eelco van Vliet";
+      userEmail = "eelcovv@gmail.com";
+    })
+  ];
 }
-
--- nixos/home/por.nix
+++ nixos/home/por.nix
@@ -1,18 +1,18 @@
 { config, pkgs, lib, ... }:
 
 {
-  home-manager.users.eelco = {
-    home.stateVersion = "24.11";
+  home.username = "por";
+  home.homeDirectory = "/home/por";
+  home.stateVersion = "24.05"; # Gebruik de juiste versie hier
 
-    imports = [
-      ./modules/common-packages.nix
+  imports = [
+    ./modules/common-packages.nix
 
-      # Eventueel persoonlijke overrides
-      (import ./modules/git.nix {
-        inherit config pkgs lib;
-        userName = "Por Mangkang";
-        userEmail = "karnrawee.mangkang@gmail.com";
-      })
-    ];
-  };
+    # Personal overrides
+    (import ./modules/git.nix {
+      inherit config pkgs lib;
+      userName = "Por Mangkang";
+      userEmail = "karnrawee.mangkang@gmail.com";
+    })
+  ];
 }
