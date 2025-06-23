677525258a40fdc3f76e8418d0f299361940621f
Eelco van Vlietl <eelco@davelab.nl>
2025-06-24 00:01:21 +0200

    now

--- nixos/home/modules/git.nix
+++ nixos/home/modules/git.nix
@@ -1,15 +1,13 @@
-# modules/git.nix
-{ config, pkgs, lib, userName ? "Set your name", userEmail ? "your@email.com", ... }:
+{ config, pkgs, lib, userName, userEmail, ... }:
 
 {
   programs.git = {
     enable = true;
-    userName = lib.mkDefault userName;
-    userEmail = lib.mkDefault userEmail;
+    userName = userName;
+    userEmail = userEmail;
     extraConfig = {
       core.editor = "vim";
       color.ui = true;
     };
   };
 }
-
