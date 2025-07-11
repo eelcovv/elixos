e012ad2cb0a9bf1c6f2d1ba4bc2d670565fd3bba
Eelco van Vlietl <eelco@davelab.nl>
2025-07-11 23:17:16 +0200

    update path

--- nixos/modules/secrets/default.nix
+++ nixos/modules/secrets/default.nix
@@ -11,11 +11,16 @@
 
   sops.secrets.age_key = {
     sopsFile = ../../secrets/age_key.yaml;
-    path = "/etc/sops/age/keys.txt";
+    path = "/run/secrets/age_key";     
     owner = "root";
     group = "root";
     mode = "0400";
   };
+
+  # Zorg dat age_key uitgerold is vóór sops-install-secrets
+  systemd.services."sops-install-secrets".requires = [ "sops-nix-age_key.service" ];
+  systemd.services."sops-install-secrets".after = [ "sops-nix-age_key.service" ];
+
 
   # Zodat systemd environment variabelen deze kunnen vinden
   systemd.services."sops-install-secrets".environment.SOPS_AGE_KEY_FILE = "/etc/sops/age/keys.txt";
