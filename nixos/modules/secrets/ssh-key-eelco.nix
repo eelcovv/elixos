2dfe537d8b113afa4218dd43c9033047479b6648
Eelco van Vlietl <eelco@davelab.nl>
2025-07-11 23:42:34 +0200

    update of file

--- nixos/modules/secrets/ssh-key-eelco.nix
+++ nixos/modules/secrets/ssh-key-eelco.nix
@@ -1,15 +1,17 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Ensure ~/.ssh exists with correct permissions before any other rules apply
+  # Ensure ~/.ssh exists with correct permissions
   systemd.tmpfiles.rules = lib.mkBefore [
     "d /home/eelco/.ssh 0700 eelco users -"
+    "L+ /home/eelco/.ssh/id_ed25519 - - - - /run/secrets/id_ed25519_eelco"
   ];
 
-  # Decrypt the private key via sops-nix
+  # Decrypt the private SSH key using sops-nix
   sops.secrets.id_ed25519_eelco = {
     sopsFile = ../../secrets/singer-eelco-secrets.yaml;
-    path = "/home/eelco/.ssh/id_ed25519";
+    key = "id_ed25519_eelco";
+    path = "/run/secrets/id_ed25519_eelco";
     owner = "eelco";
     group = "users";
     mode = "0400";
@@ -25,13 +27,12 @@
     serviceConfig = {
       Type = "oneshot";
       User = "eelco";
-      ExecStartPre = "${pkgs.coreutils}/bin/test -s /home/eelco/.ssh/id_ed25519";
+      ExecStartPre = "${pkgs.coreutils}/bin/test -s /run/secrets/id_ed25519_eelco";
       ExecStart = pkgs.writeShellScript "generate-pubkey-eelco" ''
-        ssh-keygen -y -f /home/eelco/.ssh/id_ed25519 > /home/eelco/.ssh/id_ed25519.pub
+        ssh-keygen -y -f /run/secrets/id_ed25519_eelco > /home/eelco/.ssh/id_ed25519.pub
         chown eelco:users /home/eelco/.ssh/id_ed25519.pub
         chmod 0644 /home/eelco/.ssh/id_ed25519.pub
       '';
     };
   };
 }
-
