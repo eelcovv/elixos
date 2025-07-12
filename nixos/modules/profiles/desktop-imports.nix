75f4676fe56ba3961b73c917f49849512cf600d9
Eelco van Vliet <eelcovv@gmail.com>
2025-07-12 23:32:13 +0200

    back to imports

--- nixos/modules/profiles/desktop-imports.nix
+++ nixos/modules/profiles/desktop-imports.nix
@@ -1,9 +1,7 @@
 { config, lib, ... }:
 
 {
-  imports = lib.concatLists [
-    (lib.optional config.desktop.enableGnome ./gnome.nix)
-    (lib.optional config.desktop.enableKde ./kde.nix)
-    (lib.optional config.desktop.enableHyperland ./hyperland.nix)
-  ];
+  imports = lib.optional config.desktop.enableGnome ./gnome.nix
+         ++ lib.optional config.desktop.enableKde ./kde.nix
+         ++ lib.optional config.desktop.enableHyperland ./hyperland.nix;
 }
