a8beb480071f4c7d4a97edee73102bb7d40ca981
Eelco van Vliet <eelcovv@gmail.com>
2025-07-12 22:11:04 +0200

    no settings

--- nixos/home/modules/devel/vscode.nix
+++ nixos/home/modules/devel/vscode.nix
@@ -3,7 +3,7 @@
 {
   home.packages = [ pkgs.vscode ];
 
-  xdg.configFile."Code/User/settings.json".source = ./vscode-settings.json;
+  #xdg.configFile."Code/User/settings.json".source = ./vscode-settings.json;
 
   # eventueel ook:
   # xdg.mimeApps.defaultApplications."text/plain" = "code.desktop";
