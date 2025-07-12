1aad95f77afff901d1b4f959a52b4036d52d7928
Eelco van Vlietl <eelco@davelab.nl>
2025-07-12 17:04:09 +0200

    set env

--- nixos/modules/secrets/default.nix
+++ nixos/modules/secrets/default.nix
@@ -1,17 +1,9 @@
 {
-  # systemd.tmpfiles.rules = [
-  #   "d /etc/sops/age 0700 root root -"
-  # ];
+  systemd.tmpfiles.rules = [
+    "d /etc/sops/age 0700 root root -"
+  ];
 
-  # sops.age.keyFile = "/etc/sops/age/keys.txt";
-
-  # sops.secrets.age_key_root = {
-  #   sopsFile = ../../secrets/age_key_root.yaml;
-  #   path = "/etc/sops/age/keys.txt";
-  #   owner = "root";
-  #   group = "root";
-  #   mode = "0400";
-  # };
+  sops.age.keyFile = "/etc/sops/age/keys.txt";
 
   systemd.services."sops-install-secrets".environment.SOPS_AGE_KEY_FILE = "/etc/sops/age/keys.txt";
 }
