d6d01477f8814db6ed09d63a3abec1d816e7811d
Eelco van Vliet <eelcovv@gmail.com>
2025-07-13 00:16:46 +0200

    fixed it

--- nixos/modules/profiles/desktop-configs.nix
+++ nixos/modules/profiles/desktop-configs.nix
@@ -3,26 +3,22 @@
 {
   config = lib.mkMerge [
 
-    # GNOME instellingen
     (lib.mkIf config.desktop.enableGnome (
       (import ./gnome.nix { inherit lib pkgs; }).config
     ))
 
-    # KDE instellingen
     (lib.mkIf config.desktop.enableKde (
       (import ./kde.nix { inherit lib pkgs; }).config
     ))
 
-    # Hyperland instellingen
     (lib.mkIf config.desktop.enableHyperland (
       (import ./hyperland.nix { inherit lib pkgs; }).config
     ))
 
-    # GDM alleen aanzetten als GNOME of KDE actief is
     (lib.mkIf (config.desktop.enableGnome || config.desktop.enableKde) {
-      services.displayManager.gdm.enable = true;
-      services.displayManager.gdm.autoLogin.enable = false;
-      services.displayManager.gdm.autoLogin.user = lib.mkForce null;
+      services.xserver.displayManager.gdm.enable = true;
+      services.xserver.displayManager.gdm.autoLogin.enable = false;
+      services.xserver.displayManager.gdm.autoLogin.user = lib.mkForce null;
 
       programs.ssh.askPassword = lib.mkForce "${pkgs.openssh}/libexec/ssh-askpass";
     })
