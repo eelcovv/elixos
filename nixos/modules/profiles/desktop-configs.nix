a5557cba5eeb27f83b8a63df793331260a468fdd
Eelco van Vliet <eelcovv@gmail.com>
2025-07-12 23:55:40 +0200

    another try

--- nixos/modules/profiles/desktop-configs.nix
+++ nixos/modules/profiles/desktop-configs.nix
@@ -1,14 +1,10 @@
 { config, lib, pkgs, ... }:
 
-let
-  gnome = import ./gnome.nix;
-  kde = import ./kde.nix;
-  hyperland = import ./hyperland.nix;
-in
 {
   config = lib.mkMerge (
-    lib.optional config.desktop.enableGnome (gnome { inherit config lib pkgs; }) ++
-    lib.optional config.desktop.enableKde (kde { inherit config lib pkgs; }) ++
-    lib.optional config.desktop.enableHyperland (hyperland { inherit config lib pkgs; })
+    lib.optional config.desktop.enableGnome (import ./gnome.nix { inherit pkgs lib; }) ++
+    lib.optional config.desktop.enableKde (import ./kde.nix { inherit pkgs lib; }) ++
+    lib.optional config.desktop.enableHyperland (import ./hyperland.nix { inherit pkgs lib; })
   );
 }
+
--- nixos/modules/profiles/kde.nix
+++ nixos/modules/profiles/kde.nix
@@ -1,9 +1,9 @@
-{ config, lib, pkgs, ... }: {
-  config = {
+{ lib, pkgs }: {
+  config = lib.mkIf true {
     services.xserver.enable = true;
     services.desktopManager.plasma6.enable = true;
     services.displayManager.gdm.enable = true;
+
     programs.ssh.askPassword = lib.mkForce "${pkgs.openssh}/libexec/ssh-askpass";
   };
 }
-
