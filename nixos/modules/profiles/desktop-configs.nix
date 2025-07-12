972c763eed710245be8a8708abf5212885d7d5ef
Eelco van Vliet <eelcovv@gmail.com>
2025-07-13 00:15:36 +0200

    enabled menu

--- nixos/modules/profiles/desktop-configs.nix
+++ nixos/modules/profiles/desktop-configs.nix
@@ -2,23 +2,30 @@
 
 {
   config = lib.mkMerge [
+
+    # GNOME instellingen
     (lib.mkIf config.desktop.enableGnome (
-      (import ./gnome.nix { inherit lib pkgs; }).config // {
-        services.displayManager.gdm.enable = true;
-        programs.ssh.askPassword = lib.mkForce "${pkgs.openssh}/libexec/ssh-askpass";
-      }
+      (import ./gnome.nix { inherit lib pkgs; }).config
     ))
 
+    # KDE instellingen
     (lib.mkIf config.desktop.enableKde (
-      (import ./kde.nix { inherit lib pkgs; }).config // {
-        services.displayManager.gdm.enable = true;
-        programs.ssh.askPassword = lib.mkForce "${pkgs.openssh}/libexec/ssh-askpass";
-      }
+      (import ./kde.nix { inherit lib pkgs; }).config
     ))
 
+    # Hyperland instellingen
     (lib.mkIf config.desktop.enableHyperland (
       (import ./hyperland.nix { inherit lib pkgs; }).config
     ))
+
+    # GDM alleen aanzetten als GNOME of KDE actief is
+    (lib.mkIf (config.desktop.enableGnome || config.desktop.enableKde) {
+      services.displayManager.gdm.enable = true;
+      services.displayManager.gdm.autoLogin.enable = false;
+      services.displayManager.gdm.autoLogin.user = lib.mkForce null;
+
+      programs.ssh.askPassword = lib.mkForce "${pkgs.openssh}/libexec/ssh-askpass";
+    })
   ];
 }
 
