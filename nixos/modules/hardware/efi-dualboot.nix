04524738c9dc75828bc5c111259dae66c2644e8c
Eelco van Vliet <eelcovv@gmail.com>
2025-07-13 12:56:22 +0200

    fixed dualboot

--- nixos/modules/hardware/efi-dualboot.nix
+++ nixos/modules/hardware/efi-dualboot.nix
@@ -1,9 +1,7 @@
-/**
- * Extra options to enable dual boot with windows`
- */
- { config, lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
+  # Add a custom boot entry for Windows to systemd-boot
   boot.loader.systemd-boot.extraEntries = {
     "windows.conf" = ''
       title Windows
@@ -11,6 +9,12 @@
     '';
   };
 
-  # Windows as default OS
-  boot.loader.systemd-boot.default = "windows.conf";
+  # Set Windows as the default boot entry after each install or rebuild
+  # Only if the Windows EFI bootloader is present
+  boot.loader.systemd-boot.extraInstallCommands = ''
+    if [ -e /boot/efi/EFI/Microsoft/Boot/bootmgfw.efi ]; then
+      ${pkgs.systemd}/bin/bootctl set-default windows.conf
+    fi
+  '';
 }
+
