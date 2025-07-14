9ed344e5f4aaa5f5fa254645744f2aae054518e8
Eelco van Vliet <eelcovv@gmail.com>
2025-07-14 12:27:10 +0200

    update

--- nixos/home/modules/office/nextcloud.nix
+++ nixos/home/modules/office/nextcloud.nix
@@ -1,22 +1,21 @@
 {
   pkgs,
-  config ? {url = "";},
+  lib,
+  cfg ? {url = "";},
   ...
 }: {
   programs.nextcloud-client = {
     enable = true;
     package = pkgs.nextcloud-client;
 
-    # Als je ooit sync-folders of url’s declaratief wilt meegeven:
     settings = {
       startInBackground = true;
       launchOnSystemStartup = true;
-      syncFolders = []; # ← optioneel later
+      syncFolders = [];
     };
   };
 
-  # Optioneel: een melding als de url is opgegeven (niet verplicht)
-  home.sessionVariables = lib.mkIf (config.url != "") {
-    NEXTCLOUD_URL = config.url;
+  home.sessionVariables = lib.mkIf (cfg.url != "") {
+    NEXTCLOUD_URL = cfg.url;
   };
 }
