0ce75252d657e8eb8465f4d14c67a0a405cdcb80
Eelco van Vliet <eelcovv@gmail.com>
2025-07-15 21:42:44 +0200

    morge merge

--- nixos/modules/profiles/wayland-session.nix
+++ nixos/modules/profiles/wayland-session.nix
@@ -4,11 +4,13 @@
   pkgs,
   ...
 }: {
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-    XDG_SESSION_TYPE = "wayland";
-    GDK_BACKEND = "wayland";
-    QT_QPA_PLATFORM = "wayland";
-    MOZ_ENABLE_WAYLAND = "1";
-  };
+  environment.sessionVariables = lib.mkMerge [
+    {
+      NIXOS_OZONE_WL = "1";
+      XDG_SESSION_TYPE = "wayland";
+      GDK_BACKEND = "wayland";
+      QT_QPA_PLATFORM = "wayland";
+      MOZ_ENABLE_WAYLAND = "1";
+    }
+  ];
 }
