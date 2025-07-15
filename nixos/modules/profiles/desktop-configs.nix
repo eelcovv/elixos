4a86680fc3075484cac4093fb24b288850199046
Eelco van Vliet <eelcovv@gmail.com>
2025-07-15 21:57:51 +0200

    arguments ok

--- nixos/modules/profiles/desktop-configs.nix
+++ nixos/modules/profiles/desktop-configs.nix
@@ -11,15 +11,15 @@
 
     (lib.mkIf config.desktop.enableKde (
       lib.mkMerge [
-        (import ./kde.nix {inherit lib pkgs;}).config
-        (import ./wayland-session.nix {inherit lib pkgs;})
+        (import ./kde.nix {inherit config lib pkgs;}).config
+        (import ./wayland-session.nix {inherit config lib pkgs;})
       ]
     ))
 
     (lib.mkIf config.desktop.enableHyperland (
       lib.mkMerge [
-        (import ./hyperland.nix {inherit lib pkgs;})
-        (import ./wayland-session.nix {inherit lib pkgs;})
+        (import ./hyperland.nix {inherit config lib pkgs;})
+        (import ./wayland-session.nix {inherit config lib pkgs;})
       ]
     ))
 
