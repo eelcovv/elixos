919a87ba680e469d441369ea0a92951e587c32d1
Eelco van Vliet <eelcovv@gmail.com>
2025-07-18 09:01:03 +0200

    fixwe the deffault

--- nixos/users/por.nix
+++ nixos/users/por.nix
@@ -13,4 +13,8 @@
     hashedPassword = "$6$V.Q6S5VyKvJeWOsL$c2GXEqsgBP4NocBElNAcYYV8dILH4lr3axyN9s2E5v/fhEcH/S9y/LzLxeGth6KbTEHa3LyJpKmaedKzxqCWm/";
     shell = pkgs.zsh;
   };
+
+  userMeta.defaultSession = {
+    por = "plasma";
+  };
 }
