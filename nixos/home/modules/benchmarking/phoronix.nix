ac2fae656d5005c85ebdb6e6c03c29161399701e
Eelco van Vliet <eelcovv@gmail.com>
2025-07-21 20:56:46 +0200

    updated script with install

--- nixos/home/modules/benchmarking/phoronix.nix
+++ nixos/home/modules/benchmarking/phoronix.nix
@@ -17,6 +17,17 @@
 
     echo "Benchmarkresultaten worden opgeslagen in: $OUTDIR"
 
+    # Installeer benodigde tests vooraf
+    phoronix-test-suite install \
+      pts/cpuinfo \
+      pts/compress-7zip \
+      pts/memory \
+      pts/motionmark \
+      pts/jetstream \
+      pts/octane \
+      pts/glmark2
+
+    # Draai batch benchmark
     phoronix-test-suite batch-run \
       pts/cpuinfo \
       pts/compress-7zip \
@@ -28,23 +39,5 @@
       | tee "$OUTDIR/output.txt"
   '';
   home.file.".local/bin/run-benchmarks".executable = true;
-
-  home.activation.installPhoronixConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    mkdir -p "$HOME/.phoronix-test-suite"
-    cp ${pkgs.writeText "user-config.xml" ''
-      <?xml version="1.0"?>
-      <PhoronixTestSuite>
-        <Options>
-          <SaveResults>FALSE</SaveResults>
-          <OpenBrowser>FALSE</OpenBrowser>
-          <UploadResults>FALSE</UploadResults>
-          <PromptForTestDescription>FALSE</PromptForTestDescription>
-          <PromptSaveName>FALSE</PromptSaveName>
-          <RunAllTestCombinations>FALSE</RunAllTestCombinations>
-          <RunAllTestIterations>FALSE</RunAllTestIterations>
-          <PromptSaveIdentifier>FALSE</PromptSaveIdentifier>
-        </Options>
-      </PhoronixTestSuite>
-    ''} "$HOME/.phoronix-test-suite/user-config.xml"
-  '';
 }
+
