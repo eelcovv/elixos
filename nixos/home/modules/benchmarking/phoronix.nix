75a89293aa712f8049ad2e1e7827076ebfc990c3
Eelco van Vliet <eelcovv@gmail.com>
2025-07-21 20:50:24 +0200

    added new

--- nixos/home/modules/benchmarking/phoronix.nix
+++ nixos/home/modules/benchmarking/phoronix.nix
@@ -29,21 +29,22 @@
   '';
   home.file.".local/bin/run-benchmarks".executable = true;
 
-xdg.configFile."phoronix-test-suite/user-config.xml".text = ''
-  <?xml version="1.0"?>
-  <PhoronixTestSuite>
-    <Options>
-      <SaveResults>FALSE</SaveResults>
-      <OpenBrowser>FALSE</OpenBrowser>
-      <UploadResults>FALSE</UploadResults>
-      <PromptForTestDescription>FALSE</PromptForTestDescription>
-      <PromptSaveName>FALSE</PromptSaveName>
-      <RunAllTestCombinations>FALSE</RunAllTestCombinations>
-      <RunAllTestIterations>FALSE</RunAllTestIterations>
-      <PromptSaveIdentifier>FALSE</PromptSaveIdentifier>
-    </Options>
-  </PhoronixTestSuite>
-'';
-
+  home.activation.installPhoronixConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    mkdir -p "$HOME/.phoronix-test-suite"
+    cp ${pkgs.writeText "user-config.xml" ''
+      <?xml version="1.0"?>
+      <PhoronixTestSuite>
+        <Options>
+          <SaveResults>FALSE</SaveResults>
+          <OpenBrowser>FALSE</OpenBrowser>
+          <UploadResults>FALSE</UploadResults>
+          <PromptForTestDescription>FALSE</PromptForTestDescription>
+          <PromptSaveName>FALSE</PromptSaveName>
+          <RunAllTestCombinations>FALSE</RunAllTestCombinations>
+          <RunAllTestIterations>FALSE</RunAllTestIterations>
+          <PromptSaveIdentifier>FALSE</PromptSaveIdentifier>
+        </Options>
+      </PhoronixTestSuite>
+    ''} "$HOME/.phoronix-test-suite/user-config.xml"
+  '';
 }
-
