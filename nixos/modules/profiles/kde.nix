69212f2c09035f8f63b7b2dfa91a45b5d57841c8
Eelco van Vliet <eelcovv@gmail.com>
2025-07-24 05:36:21 +0200

    added kde wallet

--- nixos/modules/profiles/kde.nix
+++ nixos/modules/profiles/kde.nix
@@ -5,12 +5,20 @@
   config = {
     services.xserver.enable = true;
     services.desktopManager.plasma6.enable = true;
-  };
 
-  environment.sessionVariables = lib.mkMerge [
-    {
+    # Activeer KWallet
+    services.kwallet.enable = true;
+
+    # Zorg dat PAM-integratie werkt
+    security.pam.services.kwallet = {
+      enable = true;
+      kwallet = true;
+      kwallet5 = true;
+    };
+
+    environment.sessionVariables = {
       XDG_CURRENT_DESKTOP = "KDE";
       XDG_SESSION_DESKTOP = "KDE";
-    }
-  ];
+    };
+  };
 }
