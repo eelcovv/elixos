344303b967ad589f2f03e51d1a4ee897497a06ac
Eelco van Vliet <eelcovv@gmail.com>
2025-07-24 05:52:01 +0200

    new config

--- nixos/modules/profiles/desktop-configs.nix
+++ nixos/modules/profiles/desktop-configs.nix
@@ -5,28 +5,33 @@
   ...
 }: {
   config = lib.mkMerge [
+    # Fonts
     (import ../fonts/default.nix {inherit pkgs;}).config
 
+    # GNOME
     (lib.mkIf config.desktop.enableGnome (
-      (import ./gnome.nix {inherit lib pkgs;}).config
+      (import ./gnome.nix {inherit lib pkgs config;}).config
     ))
 
+    # KDE Plasma
     (lib.mkIf config.desktop.enableKde (
       lib.mkMerge [
-        (import ./kde.nix {inherit lib pkgs;}).config
+        (import ./kde.nix {inherit lib pkgs config;}).config
         (import ./wayland-session.nix {inherit config lib pkgs;})
         (import ./start-keyring-daemon.nix {inherit config lib pkgs;})
       ]
     ))
 
+    # Hyprland
     (lib.mkIf config.desktop.enableHyperland (
       lib.mkMerge [
-        (import ./hyperland.nix {inherit config lib pkgs;})
+        (import ./hyperland.nix {inherit config lib pkgs;}).config
         (import ./wayland-session.nix {inherit config lib pkgs;})
         (import ./start-keyring-daemon.nix {inherit config lib pkgs;})
       ]
     ))
 
+    # GDM & SSH-askpass: alleen als één van de desktops aanstaat
     (lib.mkIf (config.desktop.enableGnome || config.desktop.enableKde || config.desktop.enableHyperland) {
       services.displayManager.gdm.enable = true;
       services.displayManager.autoLogin.enable = false;
