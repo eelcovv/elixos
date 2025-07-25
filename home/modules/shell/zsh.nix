2d777dc70c60ec8ce355fdc6cde482306485210f
Eelco van Vliet <eelcovv@gmail.com>
2025-07-25 11:19:53 +0200

    update theme

--- home/modules/shell/zsh.nix
+++ home/modules/shell/zsh.nix
@@ -5,9 +5,18 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
+    # Themas available
+    # | Theme           | Kenmerk                                                      |
+    # | --------------- | ------------------------------------------------------------ |
+    # | `agnoster`      | Git-aware, Powerline-style met segmenten, kleurrijk          |
+    # | `robbyrussell`  | Simpel, toont alleen Git-branch bij prompt                   |
+    # | `bureau`        | Zakelijk: pad, tijd, git-status                              |
+    # | `af-magic`      | Compact, toont exitcode, tijd en git                         |
+| `powerlevel10k` | Extreem configureerbaar, snel, icons, git, context (extern!) |
+
     oh-my-zsh = {
       enable = true;
-      theme = "agnoster";
+      theme = "powerlevel10k";
     };
   };
 
