c0edc169a0a0d9a3604b0205da347e115c002448
Eelco van Vliet <eelcovv@gmail.com>
2025-07-26 03:47:31 +0200

    added vim

--- home/modules/editors/vim/default.nix
+++ home/modules/editors/vim/default.nix
@@ -5,15 +5,14 @@
 }: {
   programs.vim = {
     enable = true;
-    extraConfig = ''
-      syntax on
-      set number
-      set tabstop=4
-      set shiftwidth=4
-      set expandtab
-    '';
     plugins = with pkgs.vimPlugins; [
       vim-nix
     ];
   };
+
+  home.file.".vimrc".source = ./vimrc;
+
+  home.packages = with pkgs; [
+    vim
+  ];
 }
