cedc8b3cc98186f8d1cd24ab5c80d386b36c4805
Eelco van Vliet <eelcovv@gmail.com>
2025-07-28 05:10:56 +0200

    added contabo+ secrets

--- nixos/hosts/contabo.nix
+++ nixos/hosts/contabo.nix
@@ -1,9 +1,50 @@
 {inputs, ...}: {
-  imports = [
-    ../modules/common.nix
-    ../users/users.nix
-  ];
-
   networking.hostName = "contabo";
-  system.stateVersion = "24.05";
+
+  desktop.enableGnome = true;
+  desktop.enableKde = true;
+  desktop.enableHyperland = true;
+
+  # Definine host-specifi sshUsers
+  sshUsers = ["eelco"];
+  configuredUsers = ["eelco"];
+
+  imports =
+    # 🧱 Basic modules
+    [
+      ../modules/common.nix
+      ../modules/profiles/desktop-options.nix
+      ../modules/profiles/desktop-configs.nix
+      ../modules/profiles/desktop-software.nix
+      ../modules/home-manager.nix
+    ]
+    ++
+    # 🔐 Secrets
+    [
+      ../modules/secrets/singer-eelco.nix
+    ]
+    ++
+    # 🛠️ Services
+    [
+      ../modules/services/ssh-client-keys.nix
+    ]
+    ++
+    # 💻 Hardware and disk setup
+    [
+      ../modules/hardware/bluetooth.nix
+      ../hardware/singer/configuration.nix
+      ../disks/singer.nix
+    ]
+    ++
+    # 👤 Users
+    [
+      ../users/eelco.nix
+      ../users/por.nix
+    ]
+    ++
+    # 🧩 External modules
+    [
+      inputs.disko.nixosModules.disko
+      inputs.home-manager.nixosModules.home-manager
+    ];
 }
