be95f050a18f23ce79a862f669abc36a7b5469f4
Eelco van Vliet <eelcovv@gmail.com>
2025-08-01 11:23:39 +0200

    fixed trace

--- nixos/modules/services/ssh-client-keys.nix
+++ nixos/modules/services/ssh-client-keys.nix
@@ -1,15 +1,3 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
-  options.sshUsers = lib.mkOption {
-    type = with lib.types; listOf str;
-    default = [];
-    description = "List of users for whom SSH keys should be provisioned via SOPS.";
-  };
-
   config = let
     sshUsers = config.sshUsers or [];
 
@@ -18,7 +6,7 @@
 
     validUsers = builtins.filter hasSecretFile sshUsers;
 
-    _ = builtins.trace "validUsers: ${builtins.toString validUsers}" null;
+    tracedValidUsers = builtins.trace "validUsers = ${builtins.toString validUsers}" validUsers;
 
     userSecret = user: {
       "id_ed25519_${user}" = {
@@ -33,51 +21,19 @@
 
     userService = user: {
       "generate-ssh-pubkey-${user}" = {
-        description = "Generate SSH public key for ${user}";
-        wantedBy = ["multi-user.target"];
-        after = ["sops-nix-id_ed25519_${user}.service"];
-        requires = ["sops-nix-id_ed25519_${user}.service"];
-        serviceConfig = {
-          Type = "oneshot";
-          ExecStartPre = pkgs.writeShellScript "wait-for-secret-${user}" ''
-            set -eu
-            echo "[INFO] Waiting for private key /home/${user}/.ssh/id_ed25519 to be ready..."
-            for i in $(seq 1 10); do
-              if [ -s /home/${user}/.ssh/id_ed25519 ]; then
-                echo "[INFO] Private key is available."
-                exit 0
-              fi
-              echo "[WARN] Private key not ready yet, retrying in 1s..."
-              sleep 1
-            done
-            echo "[ERROR] Timeout waiting for id_ed25519 to be available"
-            exit 1
-          '';
-          ExecStart = pkgs.writeShellScript "generate-id-ed25519-pub-${user}" ''
-            set -eu
-            echo "[INFO] Checking for ~/.ssh/id_ed25519.pub for user '${user}'"
-            if [ ! -f /home/${user}/.ssh/id_ed25519.pub ]; then
-              echo "[INFO] Generating public key for ${user}..."
-              ssh-keygen -y -f /home/${user}/.ssh/id_ed25519 > /home/${user}/.ssh/id_ed25519.pub
-              chown ${user}:users /home/${user}/.ssh/id_ed25519.pub
-              chmod 0644 /home/${user}/.ssh/id_ed25519.pub
-            else
-              echo "[INFO] Public key for ${user} already exists, skipping."
-            fi
-          '';
-        };
+        ...
       };
     };
   in {
-    sops.secrets = lib.mkMerge (map userSecret validUsers);
+    sops.secrets = lib.mkMerge (map userSecret tracedValidUsers);
 
-    systemd.services = lib.mkMerge (map userService validUsers);
+    systemd.services = lib.mkMerge (map userService tracedValidUsers);
 
     systemd.tmpfiles.rules = lib.flatten (map
       (user: [
         "d /home/${user}/.ssh 0700 ${user} users -"
         "f /home/${user}/.ssh/id_ed25519.pub 0644 ${user} users -"
       ])
-      validUsers);
+      tracedValidUsers);
   };
-}
+
