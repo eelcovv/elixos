1da0f14025cbb0c50ef0a2cc63c58253c398693a
Eelco van Vliet <eelcovv@gmail.com>
2025-08-05 12:58:50 +0200

    added comment

--- nixos/hardware/contabo/configuration.nix
+++ nixos/hardware/contabo/configuration.nix
@@ -1,3 +1,8 @@
+# this configuration is for Contabo servers using legacy BIOS boot mode
+# it sets up GRUB as the bootloader and configures the filesystem layout
+# based on a typical Ubuntu 22 setup, without separate /boot or /boot/efi
+# partitions, since this is a BIOS system
+# it also ensures /tmp is cleaned on each reboot for better security
 {
   config,
   lib,
