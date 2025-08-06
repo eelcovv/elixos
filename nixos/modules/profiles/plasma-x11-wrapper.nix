1c0e7542dee66e89887f2466aa0ffa57e7f999ad
Eelco van Vliet <eelcovv@gmail.com>
2025-08-06 16:01:47 +0200

    wrapped

--- nixos/modules/profiles/plasma-x11-wrapper.nix
+++ nixos/modules/profiles/plasma-x11-wrapper.nix
@@ -1,13 +1,8 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: let
+let
   plasmaX11Wrapped = pkgs.writeShellScript "startplasma-x11-wrapped" ''
     export XDG_SESSION_TYPE=x11
     export QT_QPA_PLATFORM=xcb
-    exec ${pkgs.plasma-workspace}/bin/startplasma-x11
+    exec ${pkgs.kdePackages.plasma-workspace}/bin/startplasma-x11
   '';
 in {
   config = lib.mkIf config.desktop.enableKde {
@@ -16,7 +11,7 @@ in {
 
     services.xserver.displayManager.session = [
       {
-        name = "plasma-x11-wrapped";
+        name = "plasma-x11";
         start = "exec ${plasmaX11Wrapped}";
         manage = "desktop";
       }
