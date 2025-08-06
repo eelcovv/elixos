82f86e277e4d0a99e3e5632fb7842e4ccae37fe7
Eelco van Vliet <eelcovv@gmail.com>
2025-08-06 14:15:12 +0200

    classic

--- nixos/modules/services/xrdp.nix
+++ nixos/modules/services/xrdp.nix
@@ -7,7 +7,8 @@
   config = {
     services.xrdp = {
       enable = true;
-      defaultWindowManager = "gnome-session";
+      #defaultWindowManager = "gnome-session";
+      defaultWindowManager = "gnome-session --session=gnome-classic";
     };
 
     networking.firewall.allowedTCPPorts = [3389];
