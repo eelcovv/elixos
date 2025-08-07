1a7e1560e9c9a4f8e888969e93a8f626cfd90f5a
Eelco van Vliet <eelcovv@gmail.com>
2025-08-07 12:17:29 +0200

    added extra deps

--- nixos/modules/profiles/gnome.nix
+++ nixos/modules/profiles/gnome.nix
@@ -5,5 +5,13 @@
   config = {
     services.xserver.enable = true;
     services.desktopManager.gnome.enable = true;
+
+    environment.systemPackages = with pkgs.kdePackages; [
+      gtk3
+      gdk-pixbuf
+      gsettings-desktop-schemas
+      at-spi2-core
+      librsvg
+    ];
   };
 }
