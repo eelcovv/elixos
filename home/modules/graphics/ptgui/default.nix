4b7064a8c3a908f557d8b515df2a8aaaf0ccfa24
Eelco van Vliet <eelcovv@gmail.com>
2025-08-08 09:08:44 +0200

    fixed ptgui

--- home/modules/graphics/ptgui/default.nix
+++ home/modules/graphics/ptgui/default.nix
@@ -1,14 +1,13 @@
 {pkgs, ...}: let
-  ptgui_src = builtins.fetchurl {
-    url = "file:///nix/store/25hxfzmwkc8nv1k6rhcm3x2ffhz7lbx0-PTGui_13.0.tar.gz";
-    sha256 = "1mvhqbr348np3in0n7wb98sbxgyrajl25rflys3k2pcd633k46ww";
-  };
-  ptgui_version = "Pro 13.0";
+  # Add the PTGui package to the store by Downloading the source tarball and running:
+  # nix store add-path PTGui_13.2.tar.gz
+  ptgui_src = /nix/store/25hxfzmwkc8nv1k6rhcm3x2ffhz7lbx0-PTGui_Pro_13.2.tar.gz;
+  ptgui_version = "Pro 13.2";
 in {
   home.packages = with pkgs; [
     (callPackage ./ptgui.nix {
-      src = /var/lib/ptgui/PTGui_Pro_13.2.tar.gz;
-      version = "Pro 13.2";
+      src = ptgui_src;
+      version = ptgui_version;
     })
   ];
 }
