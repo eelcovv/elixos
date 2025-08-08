d7a8592945394cb44057bb842c18f3c0b947a847
Eelco van Vliet <eelcovv@gmail.com>
2025-08-08 09:15:13 +0200

    added update below

--- home/modules/graphics/ptgui/default.nix
+++ home/modules/graphics/ptgui/default.nix
@@ -1,7 +1,12 @@
 {pkgs, ...}: let
-  # Add the PTGui package to the store by Downloading the source tarball and running:
-  # nix store add-path PTGui_13.2.tar.gz
-  ptgui_src = /nix/store/25hxfzmwkc8nv1k6rhcm3x2ffhz7lbx0-PTGui_Pro_13.2.tar.gz;
+  # 1. Download the PTGui source tarball
+  # 2. Add to store by running `nix store add-path PTGui_13.2.tar.gz'
+  # 3. Determine sha by `nix hash file /nix/store/blw8slhy3z8m4c5ms1s799ni8pphf9xk-PTGui_13.2.tar.gz'
+  # 4. Use the store path and sha in url and sha256 below
+  ptgui_src = builtins.fetchurl {
+    url = "file:///nix/store/blw8slhy3z8m4c5ms1s799ni8pphf9xk-PTGui_13.2.tar.gz";
+    sha256 = "UXAS06rQ10xIjf5TSqrGNjDhtz61FmVEp/732k9mMp4=";
+  };
   ptgui_version = "Pro 13.2";
 in {
   home.packages = with pkgs; [
