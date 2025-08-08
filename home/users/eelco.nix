e026b998c4b2542e51a68fbb4c870f338ffe45dd
Eelco van Vliet <eelcovv@gmail.com>
2025-08-08 17:08:38 +0200

    added keybings

--- home/users/eelco.nix
+++ home/users/eelco.nix
@@ -90,4 +90,49 @@
     ];
 
   home.sessionPath = ["$HOME/.local/bin"];
+
+  programs.dconf.enable = true;
+
+  dconf.settings = {
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/"
+      ];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      name = "Ghostty Console";
+      command = "ghostty";
+      binding = "<Super>t";
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+      name = "Google Chrome";
+      command = "google-chrome-stable";
+      binding = "<Super>b";
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
+      name = "VSCode";
+      command = "code";
+      binding = "<Super>c";
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4" = {
+      name = "WasIstLos";
+      command = "wasistlos";
+      binding = "<Super><Shift>w";
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5" = {
+      name = "KeeWeb";
+      command = "keeweb";
+      binding = "<Super>k";
+    };
+  };
 }
