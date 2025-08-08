d0814187de64b7ffbe6adbd1e1af493b86efcc2f
Eelco van Vliet <eelcovv@gmail.com>
2025-08-08 16:47:12 +0200

    added zoom

--- home/modules/office/zoom.nix
+++ home/modules/office/zoom.nix
@@ -1,28 +1,26 @@
-{ config
-, lib
-, pkgs
-, inputs
-, ...
-}:
-let
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}: let
   # Last Zoom version with working Wayland screen sharing
   zoomVersion = "6.0.2.4680";
 
   # This is nixpkgs 24.05 from the flake input which has the required pipewire v1.0.7 for older Zoom
   stablePkgs = inputs.nixpkgs-stable.legacyPackages.${pkgs.system};
 
-  zoom-us-custom = (pkgs.zoom-us.override { pipewire = stablePkgs.pipewire; }).overrideAttrs (oldAttrs: rec {
+  zoom-us-custom = (pkgs.zoom-us.override {pipewire = stablePkgs.pipewire;}).overrideAttrs (oldAttrs: rec {
     version = zoomVersion;
     src = pkgs.fetchurl {
       url = "https://zoom.us/client/${version}/zoom_x86_64.pkg.tar.xz";
       hash = "sha256-027oAblhH8EJWRXKIEs9upNvjsSFkA0wxK1t8m8nwj8=";
     };
   });
-in
-{
+in {
   home.packages = [
-    # zoom-us-custom
-    zoom-us
+    zoom-us-custom
   ];
 
   # Without disabling xwayland, fonts are pixelated and look like 💩
