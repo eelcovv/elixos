7038bd7cfb543c562aa8e826f517be1f2d9367c7
Eelco van Vliet <eelcovv@gmail.com>
2025-08-10 11:07:10 +0200

    added vscode

--- nixos/modules/services/surfshark.nix
+++ nixos/modules/services/surfshark.nix
@@ -4,39 +4,38 @@
   pkgs,
   ...
 }: {
-  # Secret for Surfshark WireGuard VPN
+  # Make sure the Surfshark WireGuard tools are available
+  environment.systemPackages = with pkgs; [wireguard-tools];
+
+  # Private key as sops-secret
   sops.secrets."surfshark/wg/privatekey" = {
-    sopsFile = ./secrets/surfshark.yaml; # pas aan naar jouw bestand
+    sopsFile = ../../secrets/surfshark.yaml; # vanuit modules/services/ naar secrets/
     format = "yaml";
-    # Zorg dat alleen root hem kan lezen:
     owner = "root";
     group = "root";
     mode = "0400";
   };
 
-  # WireGuard interface
+  # WireGuard interface voor Surfshark AMS
   networking.wireguard.interfaces.wg-surfshark = {
-    # Het /32 (of /128) adres dat Surfshark je geeft
-    ips = ["10.x.y.z/32"];
-
+    # uit jouw conf:
+    ips = ["10.14.0.2/16"];
     privateKeyFile = config.sops.secrets."surfshark/wg/privatekey".path;
 
-    # Full tunnel:
+    # twee DNS-servers uit je conf
+    dns = ["162.252.172.57" "149.154.159.92"];
+
     peers = [
       {
-        publicKey = "SERVER_PUBLIC_KEY_HIER";
-        endpoint = "ams-wg.surfshark.com:51820"; # kies jouw locatie
-        allowedIPs = ["0.0.0.0/0" "::/0"];
+        # server public key uit je conf
+        publicKey = "Lxg3jAOKcBA9tGBtB6vEWMFl5LUEB6AwOpuniYn1cig=";
+        endpoint = "nl-ams.prod.surfshark.com:51820";
+        allowedIPs = ["0.0.0.0/0"];
         persistentKeepalive = 25;
       }
     ];
-
-    # Optional: use Surfshark DNS in stead of your own
-    dns = ["SURFSHARK_DNS_IP"];
-    # mtu = 1420; # Sometime required in case of performance issues
   };
 
-  # Addons (optional but handy):
-  # Less strict reverse path filtering
+  # Handig bij full-tunnel (voorkomt te strenge RP-filter)
   networking.firewall.checkReversePath = "loose";
 }
