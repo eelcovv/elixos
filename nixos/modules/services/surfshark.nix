917cb52fe110f9a6011266a9c2b07e6b7127b6bb
Eelco van Vliet <eelcovv@gmail.com>
2025-08-11 08:11:01 +0200

    added signapore and bangkok vpn

--- nixos/modules/services/surfshark.nix
+++ nixos/modules/services/surfshark.nix
@@ -15,12 +15,11 @@
     mode = "0400";
   };
 
-  # wg-quick interface (neemt DNS over uit je conf)
-  networking.wg-quick.interfaces."wg-surfshark" = {
+  # NL (Amsterdam)
+  networking.wg-quick.interfaces."wg-surfshark-nl" = {
     address = ["10.14.0.2/16"];
     dns = ["162.252.172.57" "149.154.159.92"];
     privateKeyFile = config.sops.secrets."surfshark/wg/privatekey".path;
-
     peers = [
       {
         publicKey = "Lxg3jAOKcBA9tGBtB6vEWMFl5LUEB6AwOpuniYn1cig=";
@@ -29,9 +28,46 @@
         persistentKeepalive = 25;
       }
     ];
+    mtu = 1420;
+    autostart = false;
+  };
 
-    mtu = 1420; # helpt soms tegen vertraging
-    autostart = false; # alleen starten als jij het zegt
+  # Bangkok (Surfshark)
+  networking.wg-quick.interfaces."wg-surfshark-bkk" = {
+    address = ["10.14.0.2/16"];
+    dns = ["162.252.172.57" "149.154.159.92"];
+    privateKeyFile = config.sops.secrets."surfshark/wg/privatekey".path;
+
+    peers = [
+      {
+        publicKey = "OoFY46j/w4uQFyFu/OQ/h3x+ymJ1DJ4UR1fwGNxOxk0=";
+        endpoint = "th-bkk.prod.surfshark.com:51820";
+        allowedIPs = ["0.0.0.0/0" "::/0"];
+        persistentKeepalive = 25;
+      }
+    ];
+
+    mtu = 1420;
+    autostart = false;
+  };
+
+  # Singapore (Surfshark)
+  networking.wg-quick.interfaces."wg-surfshark-sg" = {
+    address = ["10.14.0.2/16"];
+    dns = ["162.252.172.57" "149.154.159.92"];
+    privateKeyFile = config.sops.secrets."surfshark/wg/privatekey".path;
+
+    peers = [
+      {
+        publicKey = "MGfgkhJsMVMTO33h1wr76+z6gQr/93VcGdClfbaPsnU=";
+        endpoint = "sg-sng.prod.surfshark.com:51820";
+        allowedIPs = ["0.0.0.0/0" "::/0"];
+        persistentKeepalive = 25;
+      }
+    ];
+
+    mtu = 1420;
+    autostart = false;
   };
 
   networking.firewall.checkReversePath = "loose";
