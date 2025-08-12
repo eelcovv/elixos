98c20df3f9f4dd27421d2ae32c488ea6bddd2ed2
Eelco van Vliet <eelcovv@gmail.com>
2025-08-12 05:55:12 +0200

    fixed end

--- home/modules/security/ssh-config.nix
+++ home/modules/security/ssh-config.nix
@@ -7,45 +7,46 @@
   # Generate ~/.ssh/config from this text
   programs.ssh.enable = true;
   programs.ssh.extraConfig = ''
-  # ~/.ssh/config
-  # -----------------------------------------------------------------------------
-  # Global defaults
-  # -----------------------------------------------------------------------------
-  Host *
-    IdentityFile ~/.ssh/id_ed25519
-    IdentitiesOnly yes
-    ConnectTimeout 8
-    ServerAliveInterval 15
-    ServerAliveCountMax 2
-    IPQoS none
-    Port 22
+    # ~/.ssh/config
+    # -----------------------------------------------------------------------------
+    # Global defaults
+    # -----------------------------------------------------------------------------
+    Host *
+      IdentityFile ~/.ssh/id_ed25519
+      IdentitiesOnly yes
+      ConnectTimeout 8
+      ServerAliveInterval 15
+      ServerAliveCountMax 2
+      IPQoS none
+      Port 22
 
-  # -----------------------------------------------------------------------------
-  # Contabo
-  # -----------------------------------------------------------------------------
-  Host contabo
-    HostName 194.146.13.222
-    User eelco
+    # -----------------------------------------------------------------------------
+    # Contabo
+    # -----------------------------------------------------------------------------
+    Host contabo
+      HostName 194.146.13.222
+      User eelco
 
-  Host contaboroot
-    HostName 194.146.13.222
-    User root
+    Host contaboroot
+      HostName 194.146.13.222
+      User root
 
-  # -----------------------------------------------------------------------------
-  # GitHub
-  # -----------------------------------------------------------------------------
-  Host github.com
-    HostName ssh.github.com
-    Port 443
-    User git
+    # -----------------------------------------------------------------------------
+    # GitHub
+    # -----------------------------------------------------------------------------
+    Host github.com
+      HostName ssh.github.com
+      Port 443
+      User git
 
-  Host github-443
-    HostName ssh.github.com
-    Port 443
-    User git
+    Host github-443
+      HostName ssh.github.com
+      Port 443
+      User git
 
-  Host github-22
-    HostName github.com
-    Port 22
-    User git
-'';
+    Host github-22
+      HostName github.com
+      Port 22
+      User git
+  '';
+}
