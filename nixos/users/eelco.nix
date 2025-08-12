ce1c849140e6882955174804558d6b009e721778
Eelco van Vliet <eelcovv@gmail.com>
2025-08-12 09:33:42 +0200

    fixed list

--- nixos/users/eelco.nix
+++ nixos/users/eelco.nix
@@ -34,7 +34,7 @@
     createHome = true;
     home = "/home/eelco";
     description = "Eelco van Vliet";
-    extraGroups = ["wheel" "networkmanager" "audio", "elixos"];
+    extraGroups = ["wheel" "networkmanager" "audio" "elixos"];
     hashedPassword = "$6$/BFpWvnMkSUI03E7$wZPqzCZIVxEUdf1L46hkAL.ifLlW61v4iZvWCh9MC5X9UGbRPadOg43AJrw4gfRgWwBRt0u6UxIgmuZ5KuJFo.";
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = config.authorizedKeys.perUser.eelco;
