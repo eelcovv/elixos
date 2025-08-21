ba95f75d1a4ae34976a9135ac9627056ee82c6f5
Eelco van Vliet <eelcovv@gmail.com>
2025-08-21 18:11:44 +0200

    added efi to boot

--- nixos/hardware/tongfang/configuration.nix
+++ nixos/hardware/tongfang/configuration.nix
@@ -6,7 +6,7 @@
   ...
 }: {
   imports = [
-    ../../modules/hardware/efi-boot.nix
+    ../../modules/hardware/efi-boot-at-root.nix
     ../../modules/hardware/efi-dualboot.nix
     ../../modules/hardware/gpu/nvidia.nix
     ../../modules/hardware/gpu/nvidia-wayland-env.nix
