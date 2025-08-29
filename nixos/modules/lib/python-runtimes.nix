ea999c1108a7c9726376a47bf96a9a1bee19caaf
Eelco van Vliet <eelcovv@gmail.com>
2025-08-29 21:51:13 +0200

    added runtime libs

--- nixos/modules/lib/python-runtimes.nix
+++ nixos/modules/lib/python-runtimes.nix
@@ -2,25 +2,42 @@
   pkgs,
   lib,
   ...
-}: {
+}:
+# Nix-ld runtime support for non-Nix Python wheels (NumPy, PySide6, Matplotlib, etc.)
+# Exposes common system libraries so prebuilt wheels can link correctly on NixOS.
+{
   programs.nix-ld = {
     enable = true;
+
     libraries = with pkgs; [
-      stdenv.cc.cc.lib # libstdc++.so.6 / libgcc_s.so.1
+      # C/C++ runtime
+      stdenv.cc.cc.lib # libstdc++.so.6, libgcc_s.so.1
+
+      # Core compression/crypto/glib
       zlib
       glib
+      openssl
+
+      # Fonts & text shaping
       fontconfig
       freetype
       harfbuzz
       expat
       icu
-      openssl
+
+      # Image codecs
       libpng
       libjpeg
       libtiff
+
+      # Graphics/GL
       mesa
+
+      # Wayland stack
       wayland
       libxkbcommon
+
+      # X11 stack (Qt/Matplotlib often benefit from xcb fallback)
       xorg.libX11
       xorg.libXcursor
       xorg.libXrandr
@@ -31,14 +48,18 @@
       xorg.libXcomposite
       xorg.libXext
       xorg.libXdamage
-      libxcb
-      xcbutil
-      xcbutilimage
-      xcbutilkeysyms
-      xcbutilwm
-      # Optioneel als je het tegenkomt:
-      # gfortran.cc.lib  # voor libgfortran.so
-      # vulkan-loader
+
+      # XCB utils (note the xorg.* prefixes)
+      xorg.libxcb
+      xorg.xcbutil
+      xorg.xcbutilimage
+      xorg.xcbutilkeysyms
+      xorg.xcbutilwm
+      xorg.xcbutilrenderutil
+
+      # Optional additions if needed by specific wheels:
+      # gfortran.cc.lib   # libgfortran.so.* (Fortran-backed wheels)
+      # vulkan-loader     # if you run Vulkan-backed apps/libraries
     ];
   };
 }
