cdc723d9817c83961b0ff99874124f4ac433cde7
Eelco van Vliet <eelcovv@gmail.com>
2025-08-29 21:12:42 +0200

    added pypi

--- home/modules/development/default.nix
+++ home/modules/development/default.nix
@@ -18,6 +18,16 @@
     ./rootless-podman-storage.nix
   ];
 
+  # enable PyPI integration (writes ~/.pypirc from /run/secrets/*)
+  pypi = {
+    enable = true;
+    davelab = {
+      enable = true;
+      repository = "https://pypi.davelab.eu";
+      auth.mode = "basic";
+    };
+  };
+
   home.packages = with pkgs; [
     alejandra
     htop
