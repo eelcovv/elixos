ce56ff1cdd034782a6869b9f135e0905212023e8
Eelco van Vliet <eelcovv@gmail.com>
2025-09-13 15:13:16 +0200

    minmal pib

--- nixos/modules/lib/python-runtimes.nix
+++ nixos/modules/lib/python-runtimes.nix
@@ -1,60 +1,16 @@
-# nixos/modules/lib/python-runtimes.nix
+# nixos/modules/lib/python-runtimes.nix (minimal compat)
 {
   pkgs,
   lib,
   ...
-}: let
-  # Werkt op beide nixpkgs-varianten:
-  # - pkgs.xorg.libxshmfence (meest voorkomend)
-  # - pkgs.libxshmfence (sommige channels)
-  libxshmfence_pkg = pkgs.xorg.libxshmfence or pkgs.libxshmfence;
-in {
+}: {
   programs.nix-ld.enable = true;
-
   programs.nix-ld.libraries = with pkgs; [
-    # C/C++
     stdenv.cc.cc.lib
     glibc
-    gmp # libgmp.so.10 (pymeshlab plugins)
-    p11-kit # libp11-kit.so.0 (e57 plugin chain)
-
-    # GL/GLVND
-    libglvnd
-    libGL
-    libGLU
-    mesa
-    libdrm
-
-    # Wayland/X11 (uitgebreid voor Qt xcb plugin)
-    wayland
-    libxkbcommon
-    xorg.libX11
-    xorg.libXcursor
-    xorg.libXrandr
-    xorg.libXi
-    xorg.libXrender
-    xorg.libXtst
-    xorg.libXfixes
-    xorg.libXcomposite
-    xorg.libXext
-    xorg.libXdamage
-    xorg.libxcb
-    xorg.libXinerama
-    xorg.libXxf86vm
-    libxshmfence_pkg
-
-    # xcb-util stack die Qt’s xcb-platform plugin vaak nodig heeft
-    xorg.xcbutil
-    xorg.xcbutilimage
-    xorg.xcbutilkeysyms
-    xorg.xcbutilrenderutil
-    xorg.xcbutilwm
-
-    # Core + fonts + codecs
     zlib
     glib
     openssl
-    dbus
     expat
     icu
     fontconfig
@@ -63,8 +19,6 @@ in {
     libpng
     libjpeg
     libtiff
-
-    # manylinux compat (libcom_err.so.3 → lokale alias naar .2 in je project)
     e2fsprogs
   ];
 }
