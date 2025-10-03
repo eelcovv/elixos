2527cdc5998f9168de291cc886a31b456fcb34c5
Eelco van Vliet <eelcovv@gmail.com>
2025-10-03 20:38:37 +0200

    added tree-sitter

--- home/modules/engineering/default.nix
+++ home/modules/engineering/default.nix
@@ -1,4 +1,3 @@
-# home/modules/engineering/default.nix
 {
   pkgs,
   lib,
--- home/modules/engineering/openfoam.nix
+++ home/modules/engineering/openfoam.nix
@@ -34,7 +34,18 @@ in {
   };
 
   config = mkIf cfg.enable {
-    home.packages = [pkgs.coreutils pkgs.bashInteractive];
+    home.packages = [
+      pkgs.coreutils
+      pkgs.bashInteractive
+
+      # Tooling voor tree-sitter / node-gyp builds
+      pkgs.gcc
+      pkgs.gnumake
+      pkgs.pkg-config
+      pkgs.nodejs_22
+      pkgs.tree-sitter
+      pkgs.git
+    ];
 
     # Make sure ~/.local/bin is on PATH (Home Manager usually put this, but just in case)
     home.sessionPath = ["$HOME/.local/bin"];
