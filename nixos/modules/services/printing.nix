517c320f13468a0a1013035e472f15efdd513593
Eelco van Vliet <eelcovv@gmail.com>
2025-10-08 11:52:29 +0200

    with ip

--- nixos/modules/services/printing.nix
+++ nixos/modules/services/printing.nix
@@ -2,10 +2,9 @@
 {pkgs, ...}: {
   services.printing = {
     enable = true;
-    drivers = with pkgs; [gutenprint brlaser];
     webInterface = true;
+    drivers = with pkgs; [gutenprint brlaser];
   };
-
   services.avahi = {
     enable = true;
     nssmdns4 = true;
@@ -14,18 +13,34 @@
 
   hardware.printers = {
     ensureDefaultPrinter = "Brother_DCP_L2530DW";
-
     ensurePrinters = [
       {
         name = "Brother_DCP_L2530DW";
-        # Use IPPS (TLS) and a static IP to avoid timing & mDNS issues
-        deviceUri = "ipps://192.168.1.123/ipp/print"; # <-- put your printer IP here
-        model = "everywhere"; # IPP Everywhere (driverless)
+        # Optie 1: vast IP (robuust)
+        deviceUri = "ipps://192.168.000.139/ipp/print";
+        model = "everywhere";
         ppdOptions = {
           PageSize = "A4";
           Duplex = "DuplexNoTumble";
         };
       }
+      # Of optie 2: mDNS-hostnaam zodra je die weet
+      # { name = "Brother_DCP_L2530DW";
+      #   deviceUri = "ipps://BRWXXXXXXX.local/ipp/print";
+      #   model = "everywhere";
+      #   ppdOptions = { PageSize = "A4"; Duplex = "DuplexNoTumble"; };
+      # }
     ];
+  };
+
+  # (optioneel) maak ensure-printers iets toleranter voor Wi-Fi timing
+  systemd.services.ensure-printers = {
+    after = ["NetworkManager-wait-online.service" "cups.service"];
+    wants = ["NetworkManager-wait-online.service" "cups.service"];
+    serviceConfig = {
+      Restart = "on-failure";
+      RestartSec = "15s";
+      StartLimitIntervalSec = 0;
+    };
   };
 }
