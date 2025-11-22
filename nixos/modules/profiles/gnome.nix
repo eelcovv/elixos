2a5a8ba1837ee44308c36b12e265d674a0648b65
Eelco van Vliet <eelcovv@gmail.com>
2025-11-22 09:08:27 +0100

    fixed speach

--- nixos/modules/profiles/gnome.nix
+++ nixos/modules/profiles/gnome.nix
@@ -5,7 +5,13 @@
   config = {
     services.xserver.enable = true;
     services.desktopManager.gnome.enable = true;
-    accessibility.enable = false;
+
+    environment.sessionVariables = {
+      NO_AT_BRIDGE = "1";
+      GTK_A11Y = "none";
+    };
+
+    systemd.user.services."at-spi-dbus-bus".enable = false;
 
     environment.systemPackages = with pkgs; [
       gtk3
