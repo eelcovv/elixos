9f285786093ba2b30a15d435e90d5e2b1e8035af
Eelco van Vliet <eelcovv@gmail.com>
2025-11-23 13:33:10 +0100

    added overlay

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -2,12 +2,7 @@
 #
 # This overlay adds our custom packages to the nixpkgs set.
 final: prev: {
-  # Add python packages to the python3Packages package set
-  python3Packages = prev.python3Packages.override {
-    overrides = final.lib.composeExtensions (prev.python3Packages.overrides or (_: _: {})) (
-      python-final: python-prev: {
-        capytaine = final.callPackage ./capytaine { };
-      }
-    );
-  };
+  python3Packages = prev.python3Packages.override (pyfinal: pyprev: {
+    capytaine = final.callPackage ./capytaine { };
+  });
 }
