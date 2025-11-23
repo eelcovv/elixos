b320d9dabfbc1e75f3494787d17190da0e56ff24
Eelco van Vliet <eelcovv@gmail.com>
2025-11-23 10:37:21 +0100

    added

--- home/modules/engineering/freecad.nix
+++ home/modules/engineering/freecad.nix
@@ -2,17 +2,30 @@
   pkgs,
   lib,
   ...
-}: {
+}:
+let
+  # Create a Python environment containing the 'capytaine' library required by the Ship workbench.
+  pythonWithCapytaine = pkgs.python3.withPackages (ps: [
+    ps.capytaine
+  ]);
+
+  # Create a wrapper around the freecad-wayland executable.
+  # This wrapper modifies the PYTHONPATH environment variable when FreeCAD starts,
+  # making our custom Python environment (with capytaine) available to it.
+  freecadWithWorkbenchDeps = pkgs.symlinkJoin {
+    name = "freecad-wayland-with-deps";
+    paths = [ pkgs.freecad-wayland ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/freecad --prefix PYTHONPATH : "${pythonWithCapytaine}/${pkgs.python3.sitePackages}"
+    '';
+  };
+in
+{
   # Installs FreeCAD with its workbenches and required Python dependencies.
 
-  # The 'ship' workbench requires the 'capytaine' python library.
-  # We use `withPackages` to add it to FreeCAD's Python environment.
-  home.packages = with pkgs; [
-    (freecad-wayland.withPackages
-      (ps: [
-        ps.capytaine
-      ]))
-  ];
+  # Install our newly wrapped FreeCAD package.
+  home.packages = [ freecadWithWorkbenchDeps ];
 
   # We fetch the source code of the workbenches from their GitHub repositories.
   # This is then placed in the correct directory where FreeCAD can find them.
