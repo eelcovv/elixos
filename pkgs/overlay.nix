62899a104ef30d535b954068438fc8dc0a82f50a
Eelco van Vliet <eelcovv@gmail.com>
2025-11-23 13:41:22 +0100

    fixed overlay

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -2,7 +2,26 @@
 #
 # This overlay adds our custom packages to the nixpkgs set.
 final: prev: {
-  python3Packages = prev.python3Packages.override (pyfinal: pyprev: {
-    capytaine = final.callPackage ./capytaine { };
-  });
+  python3 = prev.python3.override {
+    packageOverrides = pyfinal: pyprev: {
+      # We use final.callPackage to get access to non-python dependencies like gfortran.
+      # However, this means we must explicitly pass ALL python-specific dependencies
+      # that are not in the top-level 'final' set. We take these from 'pyfinal'.
+      capytaine = final.callPackage ./capytaine {
+        inherit (pyfinal)
+          # Build-time dependencies
+          buildPythonPackage
+          meson-python
+          oldest-supported-numpy
+          charset-normalizer
+          # Runtime dependencies
+          numpy
+          scipy
+          pandas
+          xarray
+          matplotlib
+          meshio;
+      };
+    };
+  };
 }
