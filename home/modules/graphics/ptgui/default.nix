4affe636fec9017d3319ee828cbe3c15f78f1441
Eelco van Vliet <eelcovv@gmail.com>
2025-11-28 13:52:11 +0100

    update ptgui

--- home/modules/graphics/ptgui/default.nix
+++ home/modules/graphics/ptgui/default.nix
@@ -1,17 +1,17 @@
 # ============================================
 # 🧩 PTGui Installation Instructions:
 #
-# 1. Download the PTGui tarball from www.ptgui.com (e.g. PTGui_13.2.tar.gz)
+# 1. Download the PTGui tarball from www.ptgui.com (e.g. PTGui_13.3.tar.gz)
 #
 # 2. Add the file to the Nix store:
 #
-#    nix-store --add-fixed sha256 ~/Downloads/PTGui_13.2.tar.gz
+#    nix-store --add-fixed sha256 ~/Downloads/PTGui_13.3.tar.gz
 # or
-#    nix-prefetch-url file://$HOME/Downloads/PTGui_13.2.tar.gz
+#    nix-prefetch-url file://$HOME/Downloads/PTGui_13.3.tar.gz
 #
 # 3. Determine the sha256 hash of the store path:
 #
-#    nix hash file /nix/store/blw8slhy3z8m4c5ms1s799ni8pphf9xk-PTGui_13.2.tar.gz
+#    nix hash file /nix/store/blw8slhy3z8m4c5ms1s799ni8pphf9xk-PTGui_13.3.tar.gz
 #
 # 4. Fill in the correct store path and sha256 hash below in `ptguiStorePath` and `sha256`
 #
@@ -27,11 +27,11 @@
 
   config = lib.mkIf config.programs.ptgui.enable (
     let
-      version = "Pro 13.2";
+      version = "Pro 13.3";
 
       # Attempt to require the PTGui tarball; do not fail if it has not been provided.
       _req = builtins.tryEval (pkgs.requireFile {
-        name = "PTGui_13.2.tar.gz";
+        name = "PTGui_13.3.tar.gz";
         sha256 = "sha256-UXAS06rQ10xIjf5TSqrGNjDhtz61FmVEp/732k9mMp4=";
         url = "https://www.ptgui.com/"; # informational only
       });
