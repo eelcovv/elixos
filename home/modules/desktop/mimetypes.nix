ea3a17f574f05a6dcb1ae4f24ab7cd6f10293c87
Eelco van Vliet <eelcovv@gmail.com>
2025-11-29 21:46:49 +0100

    added mimetypes

---
+++ home/modules/desktop/mimetypes.nix
@@ -0,0 +1,8 @@
+{
+  xdg.mimeApps.enable = true;
+
+  xdg.mimeApps.defaultApplications = {
+    "application/vnd.jgraph.mxfile" = [ "drawio.desktop" ];
+    "application/x-drawio" = [ "drawio.desktop" ];
+  };
+}
