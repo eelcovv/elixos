80a40d3a66fe99edc337b3d524a5aaef18eaa415
Eelco van Vliet <eelcovv@gmail.com>
2025-11-29 21:58:36 +0100

    added mime

--- home/modules/desktop/mimetypes.nix
+++ home/modules/desktop/mimetypes.nix
@@ -1,6 +1,14 @@
 {
   xdg.mimeApps.enable = true;
 
+  home.file.".local/share/mime/packages/drawio.xml" = {
+    source = ./drawio-mimetype.xml;
+    # Let home-manager's xdg.mime module handle database updates
+    onChange = ''
+      echo "Mime type for drawio added, home-manager will update the database."
+    '';
+  };
+
   xdg.mimeApps.defaultApplications = {
     "application/vnd.jgraph.mxfile" = [ "drawio.desktop" ];
     "application/x-drawio" = [ "drawio.desktop" ];
