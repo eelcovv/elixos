8f8103b234da845ad63a5669f12ac1920d6d7bc4
Eelco van Vliet <eelcovv@gmail.com>
2025-12-01 11:12:16 +0100

    turn on

--- nixos/modules/profiles/gnome.nix
+++ nixos/modules/profiles/gnome.nix
@@ -8,13 +8,6 @@
     services.gnome.gnome-initial-setup.enable = false;
     security.rtkit.enable = true;
 
-    environment.sessionVariables = {
-      NO_AT_BRIDGE = "1";
-      GTK_A11Y = "none";
-    };
-
-    systemd.user.services."at-spi-dbus-bus".enable = false;
-
     environment.systemPackages = with pkgs; [
       gtk3
       gdk-pixbuf
